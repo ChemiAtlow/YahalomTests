ee53442e6b6566be2c314e96504fcf558b865276
Chemi Atlow <chemiatlow@gmail.com>
2021-01-12 00:09:31 +0200

    React use ts & sass (#1)
    * Add sass and ts
    
    * Use Scss
    
    * use scss
    
    * TS migrate
    
    * TSify services
    
    * TSify environments
    
    * components to TSX

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Questions from "./components/questions";
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<Questions></Questions>
+		</div>
+	);
+};
+
+export default App;
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+const env = {
+	serverUrl: "http://localhost:4000",
+};
+export default env;
