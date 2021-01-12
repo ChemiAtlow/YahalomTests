acb44206525e4546aec094981a8deaa110bf7e94
Chemi Atlow <chemiatlow@gmail.com>
2021-01-12 10:45:02 +0200

    Use common in server

--- server/src/DAL/questionsRepository.ts
+++ server/src/DAL/questionsRepository.ts
@@ -1,15 +1,15 @@
 import { promises as fsPromises } from "fs";
-import { interfaces } from "../models/index";
+import type { models } from "../../../common";
 const jsonFileName = "./data/jsonAsDb.json";
 
 class DBQuestionsRepository {
-	async getAllQuestions(): Promise<interfaces.Question[]> {
+	async getAllQuestions(): Promise<models.Question[]> {
 		const questions = await fsPromises.readFile(jsonFileName, "utf8");
 		const data = JSON.parse(questions);
 		return data;
 	}
 
-	async addQuestion(question: interfaces.Question) {
+	async addQuestion(question: models.Question) {
 		let data = await this.getAllQuestions();
 		const biggestId = Math.max(...data.map(q => q.id));
 		question = { ...question, id: biggestId + 1 };
