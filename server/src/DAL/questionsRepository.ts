6efa642cecf28c87eab58271bc53365a8ff2daa8
Chemi Atlow <chemiatlow@gmail.com>
2021-01-13 13:34:25 +0200

    try more

--- server/src/DAL/questionsRepository.ts
+++ server/src/DAL/questionsRepository.ts
@@ -1,5 +1,6 @@
 import { promises as fsPromises } from "fs";
-import type { models } from "../../../common";
+// import type { models } from "../../../common";
+import { models } from "@yahalom-tests/common";
 import { DbError } from "../errors";
 const jsonFileName = "./data/jsonAsDb.json";
 
