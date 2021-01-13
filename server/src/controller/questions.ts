aa92906e4db42daa749627b9ed14c44a911cdcc9
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-14 00:12:48 +0200

    Fixed validation

--- server/src/controller/questions.ts
+++ server/src/controller/questions.ts
@@ -5,30 +5,38 @@ import { BadRequestError } from "../errors";
 // Get Questions
 export const getAllQuestions = () => {
 	return questionsRepository.getAllQuestions();
-}
+};
 
 // Add question to the list
 export const addQuestion = (question: models.interfaces.Question) => {
 	questionValidation(question);
 	return questionsRepository.addQuestion(question);
-}
+};
 
 // validation for question
 const questionValidation = (question: models.interfaces.Question) => {
-	if (!question.title) {
+	if (!question.title?.trim()) {
 		throw new BadRequestError("Question has no title");
 	}
 	if (!Array.isArray(question.answers) || !question.answers.length) {
-		throw new BadRequestError("Invalid answers");
+		throw new BadRequestError("Qeustion does not contain answers");
 	}
 
-	const correctAnswersCount = question.answers.filter(a => a.correct).length;
+	if (!question.label?.trim()) {
+		throw new BadRequestError("Question must have at least one label");
+	}
+
+	let correctAnswersCount = 0;
+	//check if answer has content and its not whitespace
+	question.answers.forEach((a, i) => {
+		if (!a.content?.trim()) {
+			throw new BadRequestError(`Answer in index ${i} has no content`);
+		}
+		if (a.correct) { correctAnswersCount++; }
+	});
+
 	if (!correctAnswersCount) {
 		throw new BadRequestError("Question must have at least one correct answer");
-	}
-
-	if (!question.label.length) {
-		throw new BadRequestError("Question must have at least one label");
 	}
 
 	question.alignment = question.alignment || "Vertical";
@@ -36,4 +44,4 @@ const questionValidation = (question: models.interfaces.Question) => {
 	question.type = correctAnswersCount === 1 ?
 		models.enums.QuestionType.SingleChoice :
 		models.enums.QuestionType.MultiChoice;
-}
+};
