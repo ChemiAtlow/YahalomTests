33e4040923228b135be12c52862e4b941e6117c7
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-14 16:12:59 +0200

    Add proxy and methods

--- server/src/DAL/questionsRepository.ts
+++ server/src/DAL/questionsRepository.ts
@@ -1,32 +1,62 @@
 import { promises as fsPromises } from "fs";
 import { models } from "@yahalom-tests/common";
-import { DbError } from "../errors";
+import { DbError, ItemNotInDbError } from "../errors";
+import { type } from "os";
 const jsonFileName = "./data/jsonAsDb.json";
 
 class DBQuestionsRepository {
+	private data?: models.interfaces.Question[];
+
+	constructor() {
+		this.getAllQuestions();
+	}
+
 	async getAllQuestions(): Promise<models.interfaces.Question[]> {
 		try {
+			if (this.data) { return this.data; }
 			const questions = await fsPromises.readFile(jsonFileName, "utf8");
 			const data = JSON.parse(questions);
-			return data;
+			if (Array.isArray(data)) {
+				this.data = data;
+				return data;
+			}
+			throw new DbError("Db is corrupt");
 		} catch (err) {
 			console.log("fetch items err", err);
+			if (err instanceof DbError) {
+				throw err;
+			}
 			throw new DbError("Couldn't fetch items");
 		}
 	}
 
+	async getQuestionById(id: models.classes.guid) {
+		const question = this.data?.find(q => q.id === id);
+		if (!question) { throw new ItemNotInDbError(id, "Question"); }
+		return question;
+	}
+
 	async addQuestion(question: models.interfaces.Question) {
 		try {
-			let data = await this.getAllQuestions(); //get all questions
 			question.id = models.classes.Guid.newGuid(); //set id
-			data.push(question);
-
-			await this.writeToFile(data);
+			this.data?.push(question);
+			await this.writeToFile(this.data || []); //if data is undefined send empty array
 			return question;
 		} catch (err) {
 			console.log("add item err", err);
 			throw new DbError("Couldn't add item");
 		}
+	}
+
+	async updateQuestion(id: models.classes.guid, question: models.interfaces.Question) {
+		console.log("updating question..");
+		let q = await this.getQuestionById(id);
+		console.log(q);
+
+		q = { ...question };
+		console.log(this.data);
+
+		//need to complete
 	}
 
 	private async writeToFile(data: models.interfaces.Question[]) {
