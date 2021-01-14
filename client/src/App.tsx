d7b3273bf8f38f414eceffe0914942d1ab4c815a
Chemi Atlow <chemiatlow@gmail.com>
2021-01-14 16:24:47 +0200

    Use routes and nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,40 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ProvideAuth } from "./hooks/auth.hook";
 import "./App.scss";
-import Questions from "./components/questions";
+import NavBar from "./components/navBar";
+import PrivateRoute from "./components/privateRoute";
+import Questions from "./views/Questions";
+import Tests from "./views/Tests";
+import Reports from "./views/Reports";
+import Login from "./views/Login";
+import Home from "./views/Home";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<Questions></Questions>
-		</div>
+		<ProvideAuth>
+			<BrowserRouter>
+				<NavBar />
+				<div className="app">
+					<Switch>
+						<PrivateRoute path="/questions">
+							<Questions />
+						</PrivateRoute>
+						<PrivateRoute path="/tests">
+							<Tests />
+						</PrivateRoute>
+						<PrivateRoute path="/reports">
+							<Reports />
+						</PrivateRoute>
+						<Route path="/login">
+							<Login />
+						</Route>
+						<PrivateRoute path="/">
+							<Home />
+						</PrivateRoute>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</ProvideAuth>
 	);
 };
 
