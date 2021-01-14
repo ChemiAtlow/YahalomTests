0f716f08405501626d9a9d474663eef4c5be2c9d
Chemi Atlow <chemiatlow@gmail.com>
2021-01-14 22:47:32 +0200

    styling + start login page

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Login.scoped.scss";
 
 const Login: React.FC = () => {
-	return <div>Login</div>;
+	return (
+		<div className="login__dialog">
+			<h1 className="login__dialog-title">Login</h1>
+		</div>
+	);
 };
 
 export default Login;
