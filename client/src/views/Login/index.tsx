2038933bea3202fcac493f0681dc0bce639e68bf
Chemi Atlow <chemiatlow@gmail.com>
2021-01-14 23:41:44 +0200

    Inital login

--- client/src/views/Login/index.tsx
+++ client/src/views/Login/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import AppButton from "../../components/Forms/Button";
+import FormField from "../../components/Forms/FormField";
 import "./Login.scoped.scss";
 
 const Login: React.FC = () => {
 	return (
 		<div className="login__dialog">
 			<h1 className="login__dialog-title">Login</h1>
+			<form onSubmit={e => e.preventDefault()}>
+				<FormField label="Email" type="text" />
+				<FormField label="Password" type="password" />
+				<AppButton type="submit">Submit</AppButton>
+			</form>
 		</div>
 	);
 };
