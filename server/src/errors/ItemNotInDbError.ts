52b282a46d4a18bcb5dda0ac5f31ff8e3b500efa
Chemi Atlow <chemiatlow@gmail.com>
2021-01-15 01:12:33 +0200

    lint

--- server/src/errors/ItemNotInDbError.ts
+++ server/src/errors/ItemNotInDbError.ts
@@ -5,7 +5,7 @@ import { HttpError } from "./HttpError";
 type ItemTypes = "Question" | "Test";
 
 export class ItemNotInDbError extends HttpError {
-    constructor(id: models.classes.guid, itemType: ItemTypes) {
-        super(HTTPStatuses.notFound, `Item not in Db: ${itemType} ${id}`);
-    }
+	constructor(id: models.classes.guid, itemType: ItemTypes) {
+		super(HTTPStatuses.notFound, `Item not in Db: ${itemType} ${id}`);
+	}
 }
