7f0d293b27d9d7a95e0e26583548542057d9a90f
Chemi Atlow <chemiatlow@gmail.com>
2021-01-14 09:49:51 +0200

    colors & logic fix

--- scripts/npm.install.js
+++ scripts/npm.install.js
@@ -1,19 +1,40 @@
 return main();
 
 async function main() {
-	console.log("Welcome to Yahalom Testim!");
+	const colors = {
+		Reset: "\x1b[0m",
+		Bright: "\x1b[1m",
+		Underscore: "\x1b[4m",
+
+		FgRed: "\x1b[31m",
+		FgGreen: "\x1b[32m",
+		FgWhite: "\x1b[37m",
+
+		BgRed: "\x1b[41m",
+		BgGreen: "\x1b[42m",
+	};
+	console.log(
+		colors.FgGreen,
+		colors.Bright,
+		"Welcome to Yahalom Testim!",
+		colors.Reset
+	);
 	const vendor = detectExecutingVendor();
 	if (vendor === "npm") {
-		console.log("");
 		const version = await versionCheck(vendor);
 		if (version.split(".")[0] < 7) {
-			console.warn(
-				`This package uses the npm workspaces feature, which is available starting npm v-7.\nthe installation might not work correctly.\nEither update your npm version, or use yarn.`
+			console.error(
+				colors.Bright,
+				colors.Underscore,
+				colors.BgRed,
+				"error",
+				colors.Reset,
+				"This package uses the npm workspaces feature, which is available starting npm v-7.\nthe installation might not work correctly.\nEither update your npm version, or use yarn."
 			);
+			process.exit(1);
 		}
-	} else {
-		console.log("We're all set, let continue!");
 	}
+	console.log(colors.BgGreen, "We're all set, let continue!", colors.Reset);
 }
 function detectExecutingVendor() {
 	const { basename } = require("path");
