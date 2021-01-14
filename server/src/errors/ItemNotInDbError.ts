c1c3edbd8853f505b18cfe6ea96b60139e193dc9
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-14 16:11:55 +0200

    Add new error type

---
+++ server/src/errors/ItemNotInDbError.ts
@@ -0,0 +1,11 @@
+import { models } from "@yahalom-tests/common";
+import { HTTPStatuses } from "../constants";
+import { HttpError } from "./HttpError";
+
+type ItemTypes = "Question" | "Test";
+
+export class ItemNotInDbError extends HttpError {
+    constructor(id: models.classes.guid, itemType: ItemTypes) {
+        super(HTTPStatuses.notFound, `Item not in Db: ${itemType} ${id}`);
+    }
+}
