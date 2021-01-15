04cf1bf1541861ad0e4a1fb91490c2264d7d8ed2
Chemi Atlow <chemiatlow@gmail.com>
2021-01-15 08:29:43 +0200

    Reem CR refactoring

--- server/src/DAL/questionsRepository.ts
+++ server/src/DAL/questionsRepository.ts
@@ -1,7 +1,6 @@
 import { promises as fsPromises } from "fs";
 import { models } from "@yahalom-tests/common";
 import { DbError, ItemNotInDbError } from "../errors";
-import { type } from "os";
 const jsonFileName = "./data/jsonAsDb.json";
 
 class DBQuestionsRepository {
@@ -13,16 +12,17 @@ class DBQuestionsRepository {
 
 	async getAllQuestions(): Promise<models.interfaces.Question[]> {
 		try {
-			if (this.data) { return this.data; }
+			if (this.data) {
+				return this.data;
+			}
 			const questions = await fsPromises.readFile(jsonFileName, "utf8");
 			const data = JSON.parse(questions);
-			if (Array.isArray(data)) {
-				this.data = data;
-				return data;
+			if (!Array.isArray(data)) {
+				throw new DbError("Db is corrupt");
 			}
-			throw new DbError("Db is corrupt");
+			this.data = data;
+			return data;
 		} catch (err) {
-			console.log("fetch items err", err);
 			if (err instanceof DbError) {
 				throw err;
 			}
@@ -31,45 +31,51 @@ class DBQuestionsRepository {
 	}
 
 	async getQuestionById(id: models.classes.guid) {
-		const question = this.data?.find(q => q.id === id);
-		if (!question) { throw new ItemNotInDbError(id, "Question"); }
-		return question;
+		const index = this.findIndexById(id);
+		return this.data![index];
 	}
 
 	async addQuestion(question: models.interfaces.Question) {
-		try {
-			question.id = models.classes.Guid.newGuid(); //set id
-			this.data?.push(question);
-			await this.writeToFile(this.data || []); //if data is undefined send empty array
-			return question;
-		} catch (err) {
-			console.log("add item err", err);
-			throw new DbError("Couldn't add item");
-		}
+		question.id = models.classes.Guid.newGuid(); //set id
+		this.data = this.data || [];
+		this.data.push(question);
+		await this.writeToFile();
+		return question;
 	}
 
-	async updateQuestion(id: models.classes.guid, question: models.interfaces.Question) {
-		let index = (this.data || []).findIndex(q => q.id === id);
-		if (!this.data || index < 0) {
-			throw new ItemNotInDbError(id, "Question");
-		}
-		this.data[index] = { ...this.data[index], ...question, id };
-		await this.writeToFile(this.data);
-		return this.data[index];
+	async updateQuestion(
+		id: models.classes.guid,
+		question: models.interfaces.Question
+	) {
+		let index = this.findIndexById(id);
+		this.data![index] = { ...this.data![index], ...question, id };
+		await this.writeToFile();
+		return this.data![index];
 	}
 
 	async deleteQuestion(id: models.classes.guid) {
+		let index = this.findIndexById(id);
+		const removed = this.data!.splice(index, 1);
+		await this.writeToFile();
+		return removed;
+	}
+
+	private findIndexById(id: models.classes.guid) {
 		let index = (this.data || []).findIndex(q => q.id === id);
 		if (!this.data || index < 0) {
 			throw new ItemNotInDbError(id, "Question");
 		}
-		const removed = this.data.splice(index, 1);
-		await this.writeToFile(this.data);
-		return removed;
+		return index;
 	}
 
-	private async writeToFile(data: models.interfaces.Question[]) {
-		await fsPromises.writeFile(jsonFileName, JSON.stringify(data));
+	private async writeToFile() {
+		try {
+			const data = JSON.stringify(this.data || []);
+			await fsPromises.writeFile(jsonFileName, data);
+		} catch (err) {
+			console.log("Writing to DB error", err);
+			throw new DbError("Couldn't write into DB.");
+		}
 	}
 }
 
