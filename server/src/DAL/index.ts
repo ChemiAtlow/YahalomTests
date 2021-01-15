9cb9c0b095742ec651f85dd828c958f22c8790a4
Chemi Atlow <chemiatlow@gmail.com>
2021-01-15 12:13:22 +0200

    Generate all repos

--- server/src/DAL/index.ts
+++ server/src/DAL/index.ts
@@ -1,6 +1,18 @@
 import type { models } from "@yahalom-tests/common";
 import { Repository } from "./reporsitory";
 
+export const organizationRepository = new Repository<models.interfaces.Organization>(
+	"organizationDB.json",
+	"Organization"
+);
+export const studyFieldRepository = new Repository<models.interfaces.StudyField>(
+	"studyFieldDB.json",
+	"StudyField"
+);
+export const userRepository = new Repository<models.interfaces.User>(
+	"userDB.json",
+	"User"
+);
 export const questionsRepository = new Repository<models.interfaces.Question>(
 	"questionDB.json",
 	"Question"
