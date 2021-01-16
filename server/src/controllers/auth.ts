719c604d2fa01f1b7e689d1dc10c27f56dc1eddd
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-17 00:03:42 +0200

    Use JWT

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,15 +1,18 @@
 import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
 import { models } from "@yahalom-tests/common";
 import { userRepository } from "../DAL";
 import { EmailTakenError } from "../errors";
 import { LoginError } from "../errors/LoginError";
 
 export const signup = async ({ email, password }: models.dtos.UserDto) => {
-	if (userRepository.getUserByEmail(email)) {
+	if (await userRepository.getUserByEmail(email)) {
 		throw new EmailTakenError(email);
 	}
 	const hashedPassword = await hash(password, 12);
-	return await userRepository.addItem({ email, password: hashedPassword });
+	const newUser = await userRepository.addItem({ email, password: hashedPassword });
+	return createUserJWT(newUser);
 };
 
 export const login = async ({ email, password }: models.dtos.UserDto) => {
@@ -21,7 +24,11 @@ export const login = async ({ email, password }: models.dtos.UserDto) => {
 	//password checkout
 	const isMatchingPass = await compare(password, userFromDb.password);
 	if (!isMatchingPass) { throw new LoginError(); }
-	return userFromDb;
+	return createUserJWT(userFromDb);
 };
 export const resetPassword = () => { };
 export const requestPasswordReset = () => { };
+
+//creats json web token
+//server knows how to read it as if it was a user
+const createUserJWT = (user: models.interfaces.User) => sign(user, "veryStrongPassword", { expiresIn: "30d" });
