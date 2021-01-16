ebe432174f801346afd42a299de447787cbdacf7
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-16 23:15:22 +0200

    Better validation

--- common/src/models/dtos/user.dto.ts
+++ common/src/models/dtos/user.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, Matches } from "class-validator";
 import { User } from "../interfaces";
+const errorMessage = "Password needs to have at least: 1 upper-case, 1 lower-case, 1 number, 1 special char and minimum 8 chars";
+
 export class UserDto implements User {
 	@IsEmail()
 	public email!: string;
 	//1 upper-case, 1 lower-case, 1 number, 1 special char, minimum 8 chars
-	@Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)
+	@Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#\$%\^&\*])(?=.{8,})/, { message: errorMessage })
 	password!: string;
 }
