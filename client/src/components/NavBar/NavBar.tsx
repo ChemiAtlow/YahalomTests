20c24d3e21218bcfed4e3004fc9ff445a2dc2081
Chemi Atlow <chemiatlow@gmail.com>
2021-01-17 17:52:24 +0200

    styling

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -6,40 +6,51 @@ import "./NavBar.scoped.scss";
 const NavBar: React.FC = () => {
 	const { jwt, signout } = useAuth();
 	return (
-		<div className="main__header">
+		<header className="main__header">
 			<div className="main__header-title">
 				<h1>Yahalom Tests</h1>
 			</div>
-			<div className="main__header-links">
+			<nav className="main__header-links">
 				{jwt ? (
 					<>
 						<Link
 							className="main__header-links__item"
 							to="/questions">
-							Manage questions
+							<div className="main__header-links__item-text">
+								Manage questions
+							</div>
+							<div className="main__header-links__item-back " />
 						</Link>
 						<Link className="main__header-links__item" to="/tests">
-							Manage tests
+							<div className="main__header-links__item-text">
+								Manage tests
+							</div>
+							<div className="main__header-links__item-back " />
 						</Link>
 						<Link
 							className="main__header-links__item"
 							to="/reports">
-							Reports
+							<div className="main__header-links__item-text">
+								Reports
+							</div>
+							<div className="main__header-links__item-back " />
 						</Link>
 						<div
 							className="main__header-links__item"
 							onClick={signout}>
-							<div className="logout-text">Logout</div>
-							<div className="logout-back" />
+							<div className="main__header-links__item-text">
+								Logout
+							</div>
+							<div className="main__header-links__item-back logout" />
 						</div>
 					</>
 				) : (
-						<Link className="main__header-links__item" to="/login">
-							Login
-						</Link>
-					)}
-			</div>
-		</div>
+					<Link className="main__header-links__item" to="/login">
+						Login
+					</Link>
+				)}
+			</nav>
+		</header>
 	);
 };
 
