1eb9adb87e1585ac8a161a3f980683f57696c7f5
Chemi Atlow <chemiatlow@gmail.com>
2021-01-17 12:04:07 +0200

    use destructuring

--- common/src/models/dtos/user.dto.ts
+++ common/src/models/dtos/user.dto.ts
@@ -1,14 +1,13 @@
 import { IsEmail, IsEmpty, Matches } from "class-validator";
 import { constants } from "@yahalom-tests/common";
 import { User } from "../interfaces";
+const { passwordDescription, passwordRegex } = constants.validations;
 
 export class UserDto implements User {
 	@IsEmail()
 	public email!: string;
 	//1 upper-case, 1 lower-case, 1 number, 1 special char, minimum 8 chars
-	@Matches(constants.validations.passwordRegex, {
-		message: constants.validations.passwordDescription,
-	})
+	@Matches(passwordRegex, { message: passwordDescription })
 	password!: string;
 	@IsEmpty()
 	role!: "CaptainAmerica" | "Admin" | "Teacher";
