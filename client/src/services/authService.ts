0c48acc2aeec96b6e586202b252fe9b136d682be
Chemi Atlow <chemiatlow@gmail.com>
2021-01-17 10:20:23 +0200

    lint

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -4,9 +4,9 @@ import http from "./httpService";
 const authRoute = "/auth/";
 
 export async function signup(user: models.interfaces.User) {
-    return await http.post<string>(`${authRoute}signup`, user);
+	return await http.post<string>(`${authRoute}signup`, user);
 }
 
 export async function login(user: models.interfaces.User) {
-    return await http.post<string>(`${authRoute}login`, user);
+	return await http.post<string>(`${authRoute}login`, user);
 }
