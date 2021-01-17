2e8c84c620f3d603019a53e1abd6e41cbd9c1a83
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-17 02:03:32 +0200

    Prepare error

--- client/src/components/Forms/FormField.tsx
+++ client/src/components/Forms/FormField.tsx
@@ -5,10 +5,12 @@ interface FormFieldProps {
 	label: string;
 	type: "text" | "password" | "number" | "textarea" | "radio" | "checkbox";
 	value: string;
+	error: string;
 	onChange: React.EventHandler<React.ChangeEvent<HTMLInputElement>>;
 }
 const FormField: React.FC<FormFieldProps> = ({
 	label,
+	error,
 	...rest
 }) => {
 	return (
