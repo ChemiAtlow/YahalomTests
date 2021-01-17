089af9d12c4c5952b4b14b86b4bfdbc4f765c496
Chemi Atlow <chemiatlow@gmail.com>
2021-01-17 10:47:49 +0200

    use consts & effect

--- client/src/views/Login/index.tsx
+++ client/src/views/Login/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
+import { constants } from "@yahalom-tests/common";
 import { AppButton, FormField } from "../../components/Forms";
 import { useAuth } from "../../hooks/auth.hook";
 import "./Login.scoped.scss";
@@ -11,7 +12,25 @@ const Login: React.FC = () => {
 	const { signin, signup } = useAuth();
 	const { from } = state || { from: { pathname: "/" } };
 	const [tmpUser, setTmpUser] = useState({ email: "", password: "" });
-	const [errors, setErrors] = useState({ email: "", password: "" });
+	const [emailError, setEmailError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const isValid = Boolean(
+		emailError || passwordError || !tmpUser.email || !tmpUser.password
+	);
+	useEffect(() => {
+		const isValidEmail = constants.validations.emailRegex.test(
+			tmpUser.email
+		);
+		setEmailError(isValidEmail ? "" : "Please enter a valid email");
+	}, [tmpUser.email, setEmailError]);
+	useEffect(() => {
+		const isValidPassword = constants.validations.passwordRegex.test(
+			tmpUser.password
+		);
+		setPasswordError(
+			isValidPassword ? "" : constants.validations.passwordDescription
+		);
+	}, [tmpUser.password, setPasswordError]);
 	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const authMethod = isLogin ? signin : signup;
@@ -25,45 +44,45 @@ const Login: React.FC = () => {
 		push(isLogin ? "/signup" : "/login", state);
 	};
 
-	const onEmailChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setTmpUser({ ...tmpUser, email: e.target.value });
-		const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(tmpUser.email);
-		setErrors({ ...errors, email: isValidEmail ? "" : "Please enter a valid email" })
-	};
-
-	const onPasswordChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setTmpUser({ ...tmpUser, password: e.target.value });
-		const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#\$%\^&\*])(?=.{8,})/.test(tmpUser.password);
-		setErrors({ ...errors, password: isValidPassword ? "" : "Password needs to have at least: 1 upper-case, 1 lower-case, 1 number, 1 special char and minimum 8 chars" });
-	};
-
-	/*  - define password regex to const in commen
-		- define error messages in consts -=> common.
-
-	*/
-
 	return (
 		<div className="login__dialog">
-			<h1 className="login__dialog-title">{isLogin ? "Login" : "Sign up"}</h1>
+			<h1 className="login__dialog-title">
+				{isLogin ? "Login" : "Sign up"}
+			</h1>
 			<form onSubmit={onSubmit}>
 				<FormField
 					label="Email"
 					type="text"
+					autoComplete="username"
 					value={tmpUser.email}
-					onChange={onEmailChanged}
-					error={errors.email}
+					onChange={e =>
+						setTmpUser({ ...tmpUser, email: e.target.value.trim() })
+					}
+					error={emailError}
 				/>
 				<FormField
 					label="Password"
 					type="password"
+					autoComplete={isLogin ? "current-password" : "new-password"}
 					value={tmpUser.password}
-					onChange={onPasswordChanged}
-					error={errors.password}
+					onChange={e =>
+						setTmpUser({
+							...tmpUser,
+							password: e.target.value.trim(),
+						})
+					}
+					error={passwordError}
 				/>
-				<AppButton disabled={Boolean((errors.email || errors.password) || (!tmpUser.email || !tmpUser.password))} type="submit">Submit</AppButton>
+				<AppButton disabled={isValid} type="submit">
+					Submit
+				</AppButton>
 			</form>
 
-			<AppButton onClick={onPageChangeRequest} type="button">{isLogin ? "Not registered? Signup!" : "Have an account? Login!"}</AppButton>
+			<AppButton onClick={onPageChangeRequest} type="button">
+				{isLogin
+					? "Not registered? Signup!"
+					: "Have an account? Login!"}
+			</AppButton>
 		</div>
 	);
 };
