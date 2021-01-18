43db27bfb26d55716ee3e5774348bb8bd8926dc4
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-18 23:12:34 +0200

    Create Ellipsis component

---
+++ client/src/components/Ellipsis/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tooltip, { direction } from '../Tooltip';
+
+interface EllipsisProps {
+    data: string;
+    maxLength: number;
+    direction: direction;
+}
+
+const Ellipsis: React.FC<EllipsisProps> = ({ data, maxLength,direction }) => {
+    const textToShow = data.length > maxLength ? `${data.substring(0, maxLength - 1).trim()}...` : data;
+
+    return (
+        <Tooltip value={data} direction={direction}>
+            <span>{textToShow}</span>
+        </Tooltip>
+    )
+}
+
+export default Ellipsis;
