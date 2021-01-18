b00aa111f80057d712cda5381d346f6b83fa99db
Chemi Atlow <chemiatlow@gmail.com>
2021-01-18 15:49:47 +0200

    try this

--- client/src/views/Questions.tsx
+++ client/src/views/Questions.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { models } from "@yahalom-tests/common";
+import React, { useEffect, useState } from "react";
+import DataTable from "../components/Table";
+import { questionService } from "../services";
 
 const Questions: React.FC = () => {
-	return <div>Questions</div>;
+	const [questions, setQuestions] = useState<models.interfaces.Question[]>(
+		[]
+	);
+
+	useEffect(() => {
+		questionService
+			.getAllQuestions()
+			.then(({ data }) => setQuestions(data));
+	}, [setQuestions]);
+	return (
+		<div>
+			<h1>Questions</h1>
+			<DataTable data={questions} />
+		</div>
+	);
 };
 
 export default Questions;
