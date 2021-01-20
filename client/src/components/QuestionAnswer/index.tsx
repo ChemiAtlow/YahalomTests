e630181160f20f7c45400c2eaadb759c0d9d9e12
Chemi Atlow <chemiatlow@gmail.com>
2021-01-21 00:51:25 +0200

    style + checked

--- client/src/components/QuestionAnswer/index.tsx
+++ client/src/components/QuestionAnswer/index.tsx
@@ -1,6 +1,7 @@
 import { models } from "@yahalom-tests/common";
 import React from "react";
 import { FormField } from "../Forms";
+import "./QuestionAnswer.scoped.scss";
 
 type EditMode = {
     isEditMode: true;
@@ -11,15 +12,17 @@ type ExamMode = {
     alignment: models.enums.Alignment;
 };
 type QuestionAnswerProps = {
-    answer: models.interfaces.Answer;
+    content: string;
+    selected: boolean;
     answerIndex: number;
     questionType: models.enums.QuestionType;
     mode: EditMode | ExamMode;
-    onSelectionChange: React.ChangeEventHandler<HTMLInputElement>
+    onSelectionChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
 const QuestionAnswer: React.FC<QuestionAnswerProps> = ({
-    answer,
+    content,
+    selected,
     answerIndex,
     questionType,
     mode,
@@ -29,17 +32,24 @@ const QuestionAnswer: React.FC<QuestionAnswerProps> = ({
         questionType === models.enums.QuestionType.MultiChoice ? "checkbox" : "radio";
     return (
         <div className={`question__answer ${mode.isEditMode ? "" : mode.alignment}`}>
-            <input type={selectionType} onChange={onSelectionChange} />
-            {mode.isEditMode ? (
-                <FormField
-                    label={`Answer ${answerIndex}`}
-                    type="textarea"
-                    value={answer.content}
-                    onChange={mode.onContentChange}
-                />
-            ) : (
-                <p>{answer.content}</p>
-            )}
+            <input
+                className="question__answer-selection"
+                type={selectionType}
+                checked={selected}
+                onChange={onSelectionChange}
+            />
+            <div className="question__answer-content">
+                {mode.isEditMode ? (
+                    <FormField
+                        label={`Answer ${answerIndex + 1}`}
+                        type="textarea"
+                        value={content}
+                        onChange={mode.onContentChange}
+                    />
+                ) : (
+                    <p>{content}</p>
+                )}
+            </div>
         </div>
     );
 };
