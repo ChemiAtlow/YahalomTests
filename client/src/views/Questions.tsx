0821ca015e12126b1b4dc315acfb4b82ffec1600
Chemi Atlow <chemiatlow@gmail.com>
2021-01-21 01:39:13 +0200

    init use

--- client/src/views/Questions.tsx
+++ client/src/views/Questions.tsx
@@ -1,69 +1,75 @@
 import { models } from "@yahalom-tests/common";
 import React, { useEffect, useState } from "react";
 import { Switch, useHistory, useRouteMatch } from "react-router-dom";
-import { Ellipsis, AppButton, DataTable, Column, ProtectedRoute } from "../components";
+import { Ellipsis, AppButton, DataTable, Column, ProtectedRoute, Icon } from "../components";
 import { questionService } from "../services";
 import { useAuth } from "../hooks";
 import EditQuestion from "./Edit Question/EditQuestion";
 
 const columns: Column[] = [
-	{
-		label: "Title",
-		isFromData: true,
-		key: "title",
-		template: ({ data }) => (
-			<Ellipsis data={data} maxLength={50} direction="right" />
-		),
-	},
-	{
-		label: "Type",
-		isFromData: true,
-		key: "type",
-		template: ({ data }) => <span>{data === 0 ? "Single choice" : "Multi choice"}</span>,
-	},
-	{
-		label: "Last Update",
-		isFromData: true,
-		key: "lastUpdate",
-		template: ({ data }) => <span>{new Date(data).toLocaleString()}</span>
-	},
-	{
-		label: "Usage count",
-		isFromData: true,
-		key: "testCount",
-		template: ({ data }) => <span>{data || 0}</span>
-	},
-	{
-		label: "",
-		isFromData: true,
-		key: "active",
-	},
+    {
+        label: "Title",
+        isFromData: true,
+        key: "title",
+        template: ({ data }) => <Ellipsis data={data} maxLength={50} direction="right" />,
+    },
+    {
+        label: "Type",
+        isFromData: true,
+        key: "type",
+        template: ({ data }) => <span>{data === 0 ? "Single choice" : "Multi choice"}</span>,
+    },
+    {
+        label: "Last Update",
+        isFromData: true,
+        key: "lastUpdate",
+        template: ({ data }) => <span>{new Date(data).toLocaleString()}</span>,
+    },
+    {
+        label: "Usage count",
+        isFromData: true,
+        key: "testCount",
+        template: ({ data }) => <span>{data || 0}</span>,
+    },
+    {
+        label: "",
+        isFromData: true,
+        key: "active",
+        template: ({ data }) => <Icon icon={data ? "active" : "trash"} />,
+    },
+    {
+        label: "",
+        isFromData: true,
+        key: "id",
+        template: ({ data }) => <Icon icon="edit" />,
+    },
 ];
 
 const Questions: React.FC = () => {
-	const [questions, setQuestions] = useState<models.interfaces.Question[]>([]);
-	const { path } = useRouteMatch();
-	const { push } = useHistory();
-	const { getOrganizationAndFieldUrl } = useAuth();
-	useEffect(() => {
-		questionService
-			.getAllQuestions()
-			.then(({ data }) => setQuestions(data));
-	}, [setQuestions]);
-	return (
-		<div>
-			<Switch>
-				<ProtectedRoute requiresField path={path} exact>
-					<h1>Questions</h1>
-					<AppButton onClick={() => push(getOrganizationAndFieldUrl("questions", "edit"))}>Add new question</AppButton>
-					<DataTable data={questions} columns={columns} />
-				</ProtectedRoute>
-				<ProtectedRoute requiresField path={`${path}/edit/:questionId?`}>
-					<EditQuestion></EditQuestion>
-				</ProtectedRoute>
-			</Switch>
-		</div>
-	);
+    const [questions, setQuestions] = useState<models.interfaces.Question[]>([]);
+    const { path } = useRouteMatch();
+    const { push } = useHistory();
+    const { getOrganizationAndFieldUrl } = useAuth();
+    useEffect(() => {
+        questionService.getAllQuestions().then(({ data }) => setQuestions(data));
+    }, [setQuestions]);
+    return (
+        <div>
+            <Switch>
+                <ProtectedRoute requiresField path={path} exact>
+                    <h1>Questions</h1>
+                    <AppButton
+                        onClick={() => push(getOrganizationAndFieldUrl("questions", "edit"))}>
+                        Add new question
+                    </AppButton>
+                    <DataTable data={questions} columns={columns} />
+                </ProtectedRoute>
+                <ProtectedRoute requiresField path={`${path}/edit/:questionId?`}>
+                    <EditQuestion />
+                </ProtectedRoute>
+            </Switch>
+        </div>
+    );
 };
 
 export default Questions;
