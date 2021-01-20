0edf0e5e42de254beaebb00ebd0669f2f8eeeb64
Chemi Atlow <chemiatlow@gmail.com>
2021-01-21 01:31:30 +0200

    create cinoonent

---
+++ client/src/components/Icon/insex.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ICONS } from "./icons";
+
+interface IconProps {
+    icon: keyof typeof ICONS;
+    size: number;
+    color: React.StyleHTMLAttributes<HTMLElement>["color"];
+}
+
+const Icon: React.FC<IconProps> = ({ icon, color = "black", size = 16 }) => (
+    <svg width={`${size}px`} height={`${size}px`} viewBox="0 0 1024 1024">
+        <path fill={color} d={ICONS[icon]} />
+    </svg>
+);
+
+export default Icon;
