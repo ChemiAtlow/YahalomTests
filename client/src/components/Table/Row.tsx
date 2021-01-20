03d44e675ca0b662149f1b9c403174ba4299f65f
Chemi Atlow <chemiatlow@gmail.com>
2021-01-20 22:31:42 +0200

    Fix tanle row

--- client/src/components/Table/Row.tsx
+++ client/src/components/Table/Row.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Column } from ".";
-import "./Row.scss";
+import "./Row.scoped.scss";
 
 const Row: React.FC<{
 	columns: Column[];
 	record: { [key: string]: any };
 }> = ({ record, columns }) => {
 	return (
-		<div className="row">
-			{columns.map((col, colInd) => (
-				<div className="col" key={`col-${colInd}`}>
-					{col.isFromData ? (
-						col.template ? (
-							<col.template data={record[col.key]} />
-						) : (
-								record[col.key]
-							)
-					) : (
-							<col.template />
-						)}
-				</div>
-			))}
-		</div>
-	);
+        <div className="table-row">
+            {columns.map((col, colInd) => (
+                <div className="col" key={`col-${colInd}`}>
+                    {col.isFromData ? (
+                        col.template ? (
+                            <col.template data={record[col.key]} />
+                        ) : (
+                            record[col.key]
+                        )
+                    ) : (
+                        <col.template />
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 };
 export default Row;
