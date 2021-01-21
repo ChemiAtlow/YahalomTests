199f4ec789678e818cab5cb8c05c9ef05ee7b851
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-21 13:10:37 +0200

    Fixed navigation

--- client/src/components/ProtectedRoute/index.tsx
+++ client/src/components/ProtectedRoute/index.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import { models } from "@yahalom-tests/common";
+import React, { useEffect, useRef } from "react";
 import {
 	Redirect,
 	Route,
 	RouteComponentProps,
 	RouteProps,
+	useParams,
 } from "react-router-dom";
 import { useAuth } from "../../hooks/";
 
 interface ProtectedRouteProps extends RouteProps {
 	requiresField?: boolean;
 	onlyNonAuth?: boolean;
+}
+interface ProtectedRouteParamsProps {
+	organizationId: string;
+	fieldId: string;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
@@ -18,18 +24,45 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
 	onlyNonAuth,
 	...rest
 }) => {
-	const { jwt, activeStudyField } = useAuth();
+	const { jwt, organizationBaseInfo, setActiveOrganization, setActiveStudyField } = useAuth();
+	const { organizationId, fieldId } = useParams<ProtectedRouteParamsProps>();
+	const isFieldAllowed = useRef(true);
+
+	useEffect(() => {
+		//need to seperate if..
+		if (requiresField) {
+			if (models.classes.Guid.isValid(organizationId || "") && models.classes.Guid.isValid(fieldId || "")) {
+				const organizationWithRequestedId = organizationBaseInfo?.find(org => org.id === organizationId)
+				if (organizationWithRequestedId) {  //user has permission to this organization
+					setActiveOrganization(organizationWithRequestedId);
+					const requestedStudyField = organizationWithRequestedId.fields.find(fld => fld.id === fieldId);
+					if (requestedStudyField) { //field is from organization
+						setActiveStudyField(requestedStudyField);
+					} else {
+						console.warn("Your organization doesn't have this study field!");
+						isFieldAllowed.current = false;
+					}
+				} else {
+					console.warn("You are not permmited to reach requested organization!");
+					isFieldAllowed.current = false;
+				}
+			}
+			else {
+				isFieldAllowed.current = false;
+			}
+		}
+	}, [setActiveOrganization, setActiveStudyField, organizationId, fieldId, requiresField, organizationBaseInfo]);
+
+
 	const isNonAuthAllowed = onlyNonAuth && !jwt;
-	const isAuthAllowed =
-		!onlyNonAuth &&
-		Boolean(jwt) &&
-		(requiresField ? Boolean(activeStudyField) : true);
+	const isAuthAllowed = !onlyNonAuth && Boolean(jwt) && isFieldAllowed;
+
 	const redirection = ({ location }: RouteComponentProps) =>
 		onlyNonAuth ? (
 			<Redirect to={{ pathname: "/" }} />
 		) : (
-			<Redirect to={{ pathname: "/login", state: { from: location } }} />
-		);
+				<Redirect to={{ pathname: "/login", state: { from: location } }} />
+			);
 	return (
 		<Route
 			{...rest}
--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,6 +1,5 @@
-import { models } from "@yahalom-tests/common";
-import React, { useEffect, useState } from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { AppButton, Select } from "../../components";
 import { useAuth } from "../../hooks";
 
@@ -19,27 +18,7 @@ const Home: React.FC = () => {
 	} = useAuth();
 	const organizations = buildItemsForSelect(organizationBaseInfo || []); //user organizations
 	const relevantFields = buildItemsForSelect(activeOrganization?.fields || []); //organization fields
-	const [linkError, setLinkError] = useState("");
-	const { state } = useLocation<{ from: { pathname: string; } }>();
 	useEffect(() => {
-		if (state?.from?.pathname) {
-			const [, organizationId, studyFieldId] = state.from.pathname.split("/");
-			if (models.classes.Guid.isValid(organizationId || "") && models.classes.Guid.isValid(studyFieldId || "")) {
-				const organizationWithRequestedId = organizationBaseInfo?.find(org => org.id === organizationId)
-				if (organizationWithRequestedId) {
-					setActiveOrganization(organizationWithRequestedId);
-					const requestedStudyField = organizationWithRequestedId.fields.find(fld => fld.id === studyFieldId);
-					if (requestedStudyField) {
-						setActiveStudyField(requestedStudyField);
-						replace(state.from.pathname)
-					} else {
-						setLinkError("Your organization doesn't have this study field!")
-					}
-				} else {
-					setLinkError("You are not permmited to reach requested organization!")
-				}
-			}
-		}
 		if (organizationBaseInfo?.length === 1) {
 			setActiveOrganization(organizationBaseInfo[0])
 			if (organizationBaseInfo[0].fields.length === 1) {
@@ -49,7 +28,7 @@ const Home: React.FC = () => {
 				replace(getOrganizationAndFieldUrl("questions"));
 			}
 		}
-	}, [state, organizationBaseInfo, getOrganizationAndFieldUrl, setActiveOrganization, setActiveStudyField, setLinkError, replace]);
+	}, [organizationBaseInfo, getOrganizationAndFieldUrl, setActiveOrganization, setActiveStudyField, replace]);
 
 	const getSelectedIndex = (e: React.ChangeEvent<HTMLSelectElement>) => e.target.selectedIndex - 1;
 	const onOrganizationChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -63,7 +42,7 @@ const Home: React.FC = () => {
 		setActiveStudyField(field);
 	};
 	const handleClick = () => {
-		replace(state?.from?.pathname || getOrganizationAndFieldUrl("questions"));
+		replace(getOrganizationAndFieldUrl("questions"));
 	};
 
 	return (
@@ -71,7 +50,6 @@ const Home: React.FC = () => {
 			<span>
 				In order to continue, select organization and field to work on.
 			</span>
-			{ linkError && <b>{linkError}</b> }
 			{ !organizationBaseInfo?.length ? <p>It seems you are not connected to any organization!</p>
 				:
 				<>
