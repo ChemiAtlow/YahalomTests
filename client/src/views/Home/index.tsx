65053099916e67db85a90bcbd9de1ec27bb0c8dc
Chemi Atlow <chemiatlow@gmail.com>
2021-01-21 09:25:36 +0200

    fix home redirection

--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { models } from "@yahalom-tests/common";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { AppButton, Select } from "../../components";
 import { useAuth } from "../../hooks";
@@ -18,19 +19,37 @@ const Home: React.FC = () => {
 	} = useAuth();
 	const organizations = buildItemsForSelect(organizationBaseInfo || []); //user organizations
 	const relevantFields = buildItemsForSelect(activeOrganization?.fields || []); //organization fields
-	const { state } = useLocation<any>();
-	const { from } = state || { from: { pathname: getOrganizationAndFieldUrl("questions") } };
+	const [linkError, setLinkError] = useState("");
+	const { state } = useLocation<{ from: { pathname: string; } }>();
 	useEffect(() => {
+		if (state?.from?.pathname) {
+			const [, organizationId, studyFieldId] = state.from.pathname.split("/");
+			if (models.classes.Guid.isValid(organizationId || "") && models.classes.Guid.isValid(studyFieldId || "")) {
+				const organizationWithRequestedId = organizationBaseInfo?.find(org => org.id === organizationId)
+				if (organizationWithRequestedId) {
+					setActiveOrganization(organizationWithRequestedId);
+					const requestedStudyField = organizationWithRequestedId.fields.find(fld => fld.id === studyFieldId);
+					if (requestedStudyField) {
+						setActiveStudyField(requestedStudyField);
+						replace(state.from.pathname)
+					} else {
+						setLinkError("Your organization doesn't have this study field!")
+					}
+				} else {
+					setLinkError("You are not permmited to reach requested organization!")
+				}
+			}
+		}
 		if (organizationBaseInfo?.length === 1) {
 			setActiveOrganization(organizationBaseInfo[0])
 			if (organizationBaseInfo[0].fields.length === 1) {
 				//There is only one organization, and one studyField, user has no buisness here.
 				//Mark his organization and studyField then move him away.
 				setActiveStudyField(organizationBaseInfo[0].fields[0]);
-				replace(from);
+				replace(getOrganizationAndFieldUrl("questions"));
 			}
 		}
-	}, [from, organizationBaseInfo, setActiveOrganization, setActiveStudyField, replace]);
+	}, [state, organizationBaseInfo, getOrganizationAndFieldUrl, setActiveOrganization, setActiveStudyField, setLinkError, replace]);
 
 	const getSelectedIndex = (e: React.ChangeEvent<HTMLSelectElement>) => e.target.selectedIndex - 1;
 	const onOrganizationChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -44,7 +63,7 @@ const Home: React.FC = () => {
 		setActiveStudyField(field);
 	};
 	const handleClick = () => {
-		replace(from);
+		replace(state?.from?.pathname || getOrganizationAndFieldUrl("questions"));
 	};
 
 	return (
@@ -52,6 +71,7 @@ const Home: React.FC = () => {
 			<span>
 				In order to continue, select organization and field to work on.
 			</span>
+			{ linkError && <b>{linkError}</b> }
 			{ !organizationBaseInfo?.length ? <p>It seems you are not connected to any organization!</p>
 				:
 				<>
