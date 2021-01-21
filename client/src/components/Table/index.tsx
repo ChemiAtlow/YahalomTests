f9d955fbc15983b2c0ac5066c2ed7f87e6902f57
Chemi Atlow <chemiatlow@gmail.com>
2021-01-21 09:38:39 +0200

    iconify sorted column

--- client/src/components/Table/index.tsx
+++ client/src/components/Table/index.tsx
@@ -1,57 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchAndSort } from "../../hooks";
+import Icon from "../Icon";
 import Row from "./Row";
 import "./Table.scoped.scss";
 
 type ArrayItem = { [key: string]: any };
 export type Column = {
-	label: string;
-	isFromData: boolean;
-	template?: React.ComponentType<{ data?: any }>;
-} & (
-		| { isFromData: true; key: string }
-		| { isFromData: false; template: React.ComponentType });
+    label: string;
+    smallColumn?: boolean;
+    template?: React.ComponentType<{ data?: any }>;
+} & ({ isFromData: true; key: string } | { isFromData: false; template: React.ComponentType });
 interface DataTableProps {
-	data: ArrayItem[];
-	columns: Column[];
+    data: ArrayItem[];
+    columns: Column[];
 }
 
 export const DataTable: React.FC<DataTableProps> = ({ data, columns }) => {
-	const { data: filteredData, sort, sortTerms } = useSearchAndSort(data);
+    const { data: filteredData, sort } = useSearchAndSort(data);
+    const [sortedColumn, setSortedColumn] = useState<{
+        col: Column;
+        direction: "Ascending" | "Descending";
+    }>();
 
-	const sortColumn = (col: Column) => {
-		if (!col.isFromData) {
-			return;
+    const sortColumn = (col: Column) => {
+        if (!col.isFromData) {
+            return;
 		}
-		let descending = false;
-		if (sortTerms.term === col.key) {
-			descending = !sortTerms.isDescending;
+        if (!sortedColumn || sortedColumn.col !== col) {
+			setSortedColumn({ col, direction: "Ascending" });
+			sort({ term: col.key, isDescending: false });
+		} else if (sortedColumn.col === col && sortedColumn.direction === "Ascending") {
+			setSortedColumn({ col, direction: "Descending" });
+			sort({ term: col.key, isDescending: true });
+		} else {
+			setSortedColumn(undefined);
+			sort(undefined)
 		}
-		sort(col.key, descending);
-	};
+	};	
 
-	return (
-		<div className="table">
-			<div className="header">
-				{columns.map((col, i) => (
-					<div key={`header-${i}`} onClick={() => sortColumn(col)}>
+    return (
+        <div className="table">
+            <div className="header">
+                {columns.map((col, i) => (
+                    <div key={`header-${i}`} onClick={() => sortColumn(col)}>
 						{col.label}
-					</div>
-				))}
-			</div>
-			<div className="body">
-				{!filteredData.length ? (
-					<div className="row row-full">No Records to show.</div>
-				) : (
-					filteredData.map((record, rowInd) => (
-						<Row
-							columns={columns}
-							record={record}
-							key={`row-${rowInd}`}
-						/>
-					))
-				)}
-			</div>
-		</div>
-	);
+						{sortedColumn?.col === col &&
+							<Icon color="white" icon={sortedColumn.direction === "Ascending" ? "sortAscending" : "sortDescending"} />
+						}
+                    </div>
+                ))}
+            </div>
+            <div className="body">
+                {!filteredData.length ? (
+                    <div className="row row-full">No Records to show.</div>
+                ) : (
+                    filteredData.map((record, rowInd) => (
+                        <Row columns={columns} record={record} key={`row-${rowInd}`} />
+                    ))
+                )}
+            </div>
+        </div>
+    );
 };
