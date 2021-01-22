c05a12f5846ab5f500e3d8cb4ab35b86c26a8771
Chemi Atlow <chemiatlow@gmail.com>
2021-01-22 14:22:11 +0200

    export midal & backdrop

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,53 +1,51 @@
-import React, { useRef } from "react";
-import { createPortal } from "react-dom";
-import { useClickOutside } from "../../hooks";
+import React from "react";
+import { ModalInstance, useClickOutside } from "../../hooks";
+import { AppButton } from "../Forms";
 import "./Modal.scoped.scss";
 
-interface ModalProps {
-    key?: string;
-    allowCloseOnClickOutside?: boolean;
+export interface ModalProps extends ModalInstance<boolean> {
     title?: React.ReactNode;
-    actions?: React.ReactNode;
-    visible: boolean;
-    onClose: () => void;
+    okText?: string;
+    cancelText?: string;
+    children: React.ReactNode;
+    closeOnClickOutside?: boolean;
 }
 
-const Modal: React.FC<ModalProps> = ({
+export const Modal: React.FC<ModalProps> = ({
     title,
-    actions,
     children,
-    key,
-    allowCloseOnClickOutside = true,
-    onClose,
-    visible,
+    close,
+    closeOnClickOutside = true,
+    cancelText,
+    okText,
 }) => {
-    const dialog = useRef<HTMLDivElement>(null);
-    useClickOutside<HTMLDivElement>({
-        callback: onClose,
-        activate: allowCloseOnClickOutside,
-        ignoredElements: [dialog],
+    const dialogRef = useClickOutside<HTMLDivElement>({
+        callback: () => close(false),
+        activate: closeOnClickOutside,
     });
     return (
-        <div>
-            {visible && createPortal(<div className="modal-backdrop" />, document.body)}
-            {visible &&
-                createPortal(
-                    <div ref={dialog} className="modal">
-                        <header className="modal-header">
-                            <div className="modal-header__title">{title}</div>
-                            <div className="modal-header__close" onClick={onClose}>
-                                &times;
-                                {/* Use Icon from different branch. this is like a machine time! */}
-                            </div>
-                        </header>
-                        <main className="modal-body">{children}</main>
-                        {actions && <footer className="modal-actions">{actions}</footer>}
-                    </div>,
-                    document.body,
-                    key
-                )}
+        <div ref={dialogRef} className="modal">
+            <header className="modal-header">
+                <div className="modal-header__title">{title}</div>
+                {/* Use Icon from different branch. this is like a machine time! */}
+                {/* <Icon onClick={() => props.closeModal()} icon='close' className="modal-header__close" /> */}
+                <div className="modal-header__close" onClick={() => close(false)}>
+                    &times;
+                </div>
+            </header>
+            <main className="modal-body">{children}</main>
+            {(okText || cancelText) && (
+                <footer className="modal-actions">
+                    {okText && <AppButton onClick={() => {
+                        close(true);
+                    }}>{okText}</AppButton>}
+                    {cancelText && (
+                        <AppButton varaiety="secondary" onClick={() => close(false)}>
+                            {cancelText}
+                        </AppButton>
+                    )}
+                </footer>
+            )}
         </div>
     );
 };
-
-export default Modal;
