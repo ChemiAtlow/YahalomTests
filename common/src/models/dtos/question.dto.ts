5bbc79d1ebb86e1f0ad715022e7455c16a3e6fe5
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-23 23:15:37 +0200

    Fix validation

--- common/src/models/dtos/question.dto.ts
+++ common/src/models/dtos/question.dto.ts
@@ -1,14 +1,14 @@
 import {
-	ArrayMinSize,
-	IsArray,
-	IsEnum,
-	IsIn,
-	IsNotEmpty,
-	IsOptional,
-	IsString,
-	IsUUID,
-	Matches,
-	ValidateNested,
+    ArrayMinSize,
+    IsArray,
+    IsEnum,
+    IsIn,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    IsUUID,
+    Matches,
+    ValidateNested,
 } from "class-validator";
 import { Type } from "class-transformer";
 import { IsWithAtLeastOneCorrectAnswer } from "../../validators";
@@ -18,28 +18,27 @@ import { AnswerDto } from "./answer.dto";
 import "reflect-metadata";
 
 export class QuestionDto {
-           @IsOptional()
-           @IsUUID("4")
-           public id?: guid;
-           @IsString()
-           @IsNotEmpty()
-           public title!: string;
-           @IsOptional()
-           @IsString()
-           additionalContent?: string;
-           @IsEnum(QuestionType)
-           type!: QuestionType;
-           @IsArray()
-           @ArrayMinSize(2)
-           @Type(() => AnswerDto)
-           @ValidateNested({ each: true, message: "Answers are invalid" })
-           @IsWithAtLeastOneCorrectAnswer()
-           answers!: AnswerDto[];
-           @Matches(/(\w+)(,\s*\w+)*/, {
-               message: "Label must be a comma seperated string!",
-           })
-           label!: string;
-           @IsString()
-           @IsEnum(Alignment)
-           alignment!: Alignment;
-       }
+    @IsOptional()
+    @IsUUID("4")
+    public id?: guid;
+    @IsString()
+    @IsNotEmpty()
+    public title!: string;
+    @IsOptional()
+    @IsString()
+    additionalContent?: string;
+    @IsEnum(QuestionType)
+    type!: QuestionType;
+    @IsArray()
+    @ArrayMinSize(2)
+    @Type(() => AnswerDto)
+    @ValidateNested({ each: true, message: "Answers are invalid" })
+    @IsWithAtLeastOneCorrectAnswer()
+    answers!: AnswerDto[];
+    @Matches(/(\w+)(,\s*\w+)*/, {
+        message: "Label must be a comma seperated string!",
+    })
+    label!: string;
+    @IsEnum(Alignment)
+    alignment!: Alignment;
+}
