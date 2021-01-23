793d9f79b89e9635ddb36abc3c2f4939fbad7f81
Chemi Atlow <chemiatlow@gmail.com>
2021-01-23 21:53:41 +0200

    Make Icon clickable

--- client/src/components/Icon/index.tsx
+++ client/src/components/Icon/index.tsx
@@ -6,10 +6,16 @@ interface IconProps {
     icon: keyof typeof ICONS;
     size?: number;
     color?: React.StyleHTMLAttributes<HTMLElement>["color"];
+    onClick?: React.MouseEventHandler<SVGElement>;
 }
 
-const Icon: React.FC<IconProps> = ({ icon, color = "black", size = 16 }) => (
-    <svg className="icon" width={`${size}px`} height={`${size}px`} viewBox="0 0 1024 1024">
+const Icon: React.FC<IconProps> = ({ icon, color = "black", size = 16, onClick }) => (
+    <svg
+        className={`icon ${onClick ? "clickable" : ""}`}
+        width={`${size}px`}
+        height={`${size}px`}
+        viewBox="0 0 1024 1024"
+        onClick={onClick}>
         <path fill={color} d={ICONS[icon]} />
     </svg>
 );
