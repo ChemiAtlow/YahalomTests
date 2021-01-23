f922d5d51501a7829480978c4c12edca0be90373
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 01:33:18 +0200

    simplify

--- server/src/services/field.service.ts
+++ server/src/services/field.service.ts
@@ -2,17 +2,23 @@ import { models } from "@yahalom-tests/common";
 import { studyFieldRepository } from "../DAL";
 import { ItemNotInDbError } from "../errors";
 
-export const getStudyFieldById = async (id: models.classes.guid) =>
-    await studyFieldRepository.getItemById(id);
+export const getStudyFieldById = async (id: models.classes.guid) => {
+    const field = await studyFieldRepository.getItemById(id);
+    if (!field) {
+        throw new ItemNotInDbError(id, "StudyField");
+    }
+    return field;
+};
 
 export const getQuestionIdsByField = async (fieldId: models.classes.guid) => {
-    const field = await studyFieldRepository.getItemById(fieldId);
-    if (!field) { throw new ItemNotInDbError(fieldId, "StudyField"); }
+    const field = await getStudyFieldById(fieldId);
     return field.questions;
 };
 
-export const addQuestion = async (fieldId: models.classes.guid, questionId: models.classes.guid) => {
+export const addQuestion = async (
+    fieldId: models.classes.guid,
+    questionId: models.classes.guid
+) => {
     const field = await getStudyFieldById(fieldId);
-    if (!field) { throw new ItemNotInDbError(fieldId, "StudyField"); }
     studyFieldRepository.updateItem(fieldId, { questions: [...field.questions, questionId] });
 };
--- server/src/services/organization.service.ts
+++ server/src/services/organization.service.ts
@@ -2,23 +2,36 @@ import { models } from "@yahalom-tests/common";
 import { organizationRepository } from "../DAL";
 import { ItemNotInDbError } from "../errors";
 
-export const getOrganizationById = async (id: models.classes.guid) =>
-    await organizationRepository.getItemById(id);
-
-export const addQuestion = async (organizationId: models.classes.guid, questionId: models.classes.guid) => {
-    const organization = await getOrganizationById(organizationId);
-    if (!organization) { throw new ItemNotInDbError(organizationId, "Organization"); }
-    organizationRepository.updateItem(organizationId, { questions: [...organization.questions, questionId] });
+export const getOrganizationById = async (id: models.classes.guid) => {
+    const organization = await organizationRepository.getItemById(id);
+    if (!organization) {
+        throw new ItemNotInDbError(id, "Organization");
+    }
+    return organization;
 };
 
-export const isFieldConnectedToOrganization = async (organizationId: models.classes.guid, fieldId: models.classes.guid) => {
+export const addQuestion = async (
+    organizationId: models.classes.guid,
+    questionId: models.classes.guid
+) => {
     const organization = await getOrganizationById(organizationId);
-    if (!organization) { throw new ItemNotInDbError(organizationId, "Organization"); }
+    organizationRepository.updateItem(organizationId, {
+        questions: [...organization.questions, questionId],
+    });
+};
+
+export const isFieldConnectedToOrganization = async (
+    organizationId: models.classes.guid,
+    fieldId: models.classes.guid
+) => {
+    const organization = await getOrganizationById(organizationId);
     return organization.fields.includes(fieldId);
 };
 
-export const isUserConnectedToOrganization = async (organizationId: models.classes.guid, userdId: models.classes.guid) => {
+export const isUserConnectedToOrganization = async (
+    organizationId: models.classes.guid,
+    userdId: models.classes.guid
+) => {
     const organization = await getOrganizationById(organizationId);
-    if (!organization) { throw new ItemNotInDbError(organizationId, "Organization"); }
     return organization.users.includes(userdId);
 };
