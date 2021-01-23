21a45b66622794457da606b125427ec7d208d9c0
Chemi Atlow <chemiatlow@gmail.com>
2021-01-23 21:56:44 +0200

    add column buttons functionality, and use the auth data

--- client/src/views/Questions/index.tsx
+++ client/src/views/Questions/index.tsx
@@ -14,72 +14,82 @@ import { questionService } from "../../services";
 import { useAuth } from "../../hooks";
 import EditQuestion from "./EditQuestion";
 
-const columns: Column[] = [
-    {
-        label: "Title",
-        isFromData: true,
-        key: "title",
-        sortable: true,
-        largeColumn: true,
-        template: ({ data }) => <Ellipsis data={data} maxLength={50} direction="right" />,
-    },
-    {
-        label: "Type",
-        isFromData: true,
-        key: "type",
-        sortable: true,
-        template: ({ data }) => <span>{data === 0 ? "Single choice" : "Multi choice"}</span>,
-    },
-    {
-        label: "Last Update",
-        isFromData: true,
-        key: "lastUpdate",
-        sortable: true,
-        template: ({ data }) => <span>{new Date(data).toLocaleString()}</span>,
-    },
-    {
-        label: "Usage count",
-        isFromData: true,
-        key: "testCount",
-        sortable: true,
-        template: ({ data }) => <span>{data || 0}</span>,
-    },
-    {
-        label: "",
-        isFromData: true,
-        key: "*",
-        sortable: false,
-        smallColumn: true,
-        template: ({ data }) => (
-            <Tooltip
-                value={data.active ? "Question is active" : "Remove question."}
-                direction="left">
-                <Icon icon={data.active ? "active" : "trash"} />
-            </Tooltip>
-        ),
-    },
-    {
-        label: "",
-        isFromData: true,
-        key: "id",
-        sortable: false,
-        smallColumn: true,
-        template: ({ data }) => (
-            <Tooltip value="Click to edit the question." direction="left">
-                <Icon icon="edit" />
-            </Tooltip>
-        ),
-    },
-];
-
 const Questions: React.FC = () => {
     const [questions, setQuestions] = useState<models.interfaces.Question[]>([]);
     const { path } = useRouteMatch();
     const { push } = useHistory();
-    const { getOrganizationAndFieldUrl } = useAuth();
+    const { getOrganizationAndFieldUrl, buildAuthRequestData } = useAuth();
+    const reomveQuestion = (id: models.classes.guid) => {
+        console.log("I have to remove ");
+    };
+    const goToEditQuestion = (id: models.classes.guid) =>
+        push(getOrganizationAndFieldUrl("questions", "edit", id));
+    const columns: Column[] = [
+        {
+            label: "Title",
+            isFromData: true,
+            key: "title",
+            sortable: true,
+            largeColumn: true,
+            template: ({ data }) => <Ellipsis data={data} maxLength={50} direction="right" />,
+        },
+        {
+            label: "Type",
+            isFromData: true,
+            key: "type",
+            sortable: true,
+            template: ({ data }) => <span>{data === 0 ? "Single choice" : "Multi choice"}</span>,
+        },
+        {
+            label: "Last Update",
+            isFromData: true,
+            key: "lastUpdate",
+            sortable: true,
+            template: ({ data }) => <span>{new Date(data).toLocaleString()}</span>,
+        },
+        {
+            label: "Usage count",
+            isFromData: true,
+            key: "testCount",
+            sortable: true,
+            template: ({ data }) => <span>{data || 0}</span>,
+        },
+        {
+            label: "",
+            isFromData: true,
+            key: "*",
+            sortable: false,
+            smallColumn: true,
+            template: ({ data }) => (
+                <Tooltip
+                    value={data.active ? "Question is active" : "Remove question."}
+                    direction="left">
+                    <Icon
+                        icon={data.active ? "active" : "trash"}
+                        onClick={data.active ? undefined : () => reomveQuestion(data.id)}
+                    />
+                </Tooltip>
+            ),
+        },
+        {
+            label: "",
+            isFromData: true,
+            key: "id",
+            sortable: false,
+            smallColumn: true,
+            template: ({ data }) => (
+                <Tooltip value="Click to edit the question." direction="left">
+                    <Icon icon="edit" onClick={() => goToEditQuestion(data)} />
+                </Tooltip>
+            ),
+        },
+    ];
+
     useEffect(() => {
-        questionService.getAllQuestions().then(({ data }) => setQuestions(data));
-    }, [setQuestions]);
+        questionService
+            .getAllQuestions(buildAuthRequestData())
+            .then(({ data }) => setQuestions(data));
+    }, [setQuestions, buildAuthRequestData]);
     return (
         <div>
             <Switch>
