af95edfeeb78f1ff6d41fbc0ef590e1add6d3e1c
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 09:36:25 +0200

    Add auth service

---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,97 @@
+import { hash, compare } from "bcryptjs";
+import { randomBytes } from "crypto";
+import { sign } from "jsonwebtoken";
+import { models } from "@yahalom-tests/common";
+import { userRepository } from "../DAL";
+import { EmailTakenError, AuthError, HttpError } from "../errors";
+import { HTTPStatuses, TIME, general } from "../constants";
+import { emailService, fieldService, organizationService } from ".";
+
+export const signup = async ({ email, password }: models.dtos.UserDto) => {
+    if (await userRepository.getUserByEmail(email)) {
+        throw new EmailTakenError(email);
+    }
+    const hashedPassword = await hash(password, 12);
+    await userRepository.addItem({
+        email,
+        password: hashedPassword,
+        role: "Teacher",
+    });
+    await emailService.sendSignUpEmail(email);
+};
+
+export const login = async ({ email, password }: models.dtos.UserDto) => {
+    //check if user exist
+    const userFromDb = await userRepository.getUserByEmail(email);
+    if (!userFromDb) {
+        throw new AuthError();
+    }
+    //check if encrypted password and password are matching
+    const isMatchingPass = await compare(password, userFromDb.password);
+    if (!isMatchingPass) {
+        throw new AuthError();
+    }
+    const jwt = createUserJWT(userFromDb);
+    //Get info of organization and study fields.
+    const organizationsInfo = await buildUserOrganizationAndStudyFieldData(userFromDb.id!);
+    return { jwt, organizationsInfo };
+};
+
+export const requestPasswordReset = async ({ email }: models.dtos.RequestPasswordResetDto) => {
+    //check if user exist
+    const userFromDb = await userRepository.getUserByEmail(email);
+    if (!userFromDb) {
+        throw new AuthError();
+    }
+    //Try generating reset token
+    try {
+        const token = randomBytes(32).toString("hex");
+        await userRepository.updateItem(userFromDb.id!, {
+            resetToken: token,
+            resetTokenExpiration: Date.now() + TIME.month,
+        });
+        await emailService.sendPasswordResetEmail(email, token);
+    } catch (err) {
+        throw new HttpError(
+            HTTPStatuses.internalServerError,
+            "An unhandeled error happened when creating reset token."
+        );
+    }
+};
+
+export const resetPassword = async (token: string, { password }: models.dtos.ResetPasswordDto) => {
+    //check if user exist
+    const userFromDb = await userRepository.getUserWithResetToken(token);
+    if (!userFromDb) {
+        throw new AuthError();
+    }
+    //If user was found, his token is valid, lets reset him!
+    const hashedPassword = await hash(password, 12);
+    await userRepository.updateItem(userFromDb.id!, {
+        resetToken: undefined,
+        resetTokenExpiration: undefined,
+        password: hashedPassword,
+    });
+};
+
+const buildUserOrganizationAndStudyFieldData = async (userId: models.classes.guid) => {
+    const organizations = await organizationService.getAllOrganizations();
+    const userOrganizatios = organizations.filter(org => org.users.includes(userId));
+    return await Promise.all(
+        userOrganizatios.map<Promise<models.interfaces.OrganizationBaseInfo>>(
+            async ({ fields: fieldIds, name, id }) => {
+                const fields = await fieldService.getStudyFieldsBaseDataByIds(fieldIds);
+                return {
+                    id: id as models.classes.guid,
+                    name,
+                    fields,
+                };
+            }
+        )
+    );
+};
+
+//creats json web token
+//server knows how to read it as if it was a user
+const createUserJWT = (user: models.interfaces.User) =>
+    sign(user, general.jwtSecret, { expiresIn: "30d" });
--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -3,3 +3,4 @@ export * as organizationService from './organization.service';
 export * as fieldService from './field.service';
 export * as testService from './test.service';
 export * as questionService from './question.service';
+export * as authService from './auth.service';
