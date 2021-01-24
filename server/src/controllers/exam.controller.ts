08d41bb6b40acf1859c3f28695050206e607419d
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 22:53:21 +0200

    start an exam controller with certificate

---
+++ server/src/controllers/exam.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { certificationService } from "../services";
+
+export const getExamPassedCertificate = (req: Request, res: Response) => {
+    //TODO: Validate exam was actually passed successfully, and gather info for certificate.
+    const doc = certificationService.createCertificate({
+        firstName: "Reem",
+        lastName: "Cohen",
+    });
+    doc.pipe(res);
+    doc.end();
+}
