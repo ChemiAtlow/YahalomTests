e2677f624f5128e7ecc5a9b78efcbdd5ce7cbb13
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 15:56:35 +0200

    naming

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,3 +1,3 @@
-export { router as questionsRouter } from "./questionRoutes";
-export { router as authRouter } from "./authRoutes";
+export { router as questionsRouter } from "./question.routes";
+export { router as authRouter } from "./auth.routes";
 export { router as testRoutes } from "./test.routes";
