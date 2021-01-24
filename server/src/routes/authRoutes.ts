93abeed9a56fdcc2c31fdf3028d79271ef0b025c
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 09:36:54 +0200

    Route -> Controller -> Service

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -2,45 +2,21 @@ import { Router } from "express";
 import { models } from "@yahalom-tests/common";
 import { authController } from "../controllers";
 import { validationMiddleware } from "../middleware";
-import { HTTPStatuses } from "../constants";
 
 export const router = Router();
 
+router.post("/login", validationMiddleware(models.dtos.UserDto), authController.login);
+
+router.post("/signup", validationMiddleware(models.dtos.UserDto), authController.signup);
+
 router.post(
-	"/login",
-	validationMiddleware(models.dtos.UserDto),
-	async (req, res) => {
-		//send user to login.
-		const jwtWithOrgAndFields = await authController.login(req.body);
-		res.send(jwtWithOrgAndFields);
-	}
+    "/reset",
+    validationMiddleware(models.dtos.RequestPasswordResetDto),
+    authController.requestPasswordReset
 );
 
 router.post(
-	"/signup",
-	validationMiddleware(models.dtos.UserDto),
-	async (req, res) => {
-		await authController.signup(req.body);
-		res.status(HTTPStatuses.created).send({
-			message: "user created successfully",
-		});
-	}
-);
-
-router.post(
-	"/reset",
-	validationMiddleware(models.dtos.RequestPasswordResetDto),
-	async (req, res) => {
-		await authController.requestPasswordReset(req.body);
-		res.send({ message: "token sent to email" });
-	}
-);
-
-router.post(
-	"/reset/:token",
-	validationMiddleware(models.dtos.ResetPasswordDto),
-	async (req, res) => {
-		await authController.resetPassword(req.params.token, req.body);
-		res.send({ message: "password reset success" });
-	}
+    "/reset/:token",
+    validationMiddleware(models.dtos.ResetPasswordDto),
+    authController.resetPassword
 );
