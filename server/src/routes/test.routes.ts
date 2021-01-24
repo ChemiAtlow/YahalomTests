ef5a30b85960e7f9b435306a0d9d8839737e517e
Chemi Atlow <chemiatlow@gmail.com>
2021-01-24 12:03:32 +0200

    Auth middleware was moved to app

--- server/src/routes/test.routes.ts
+++ server/src/routes/test.routes.ts
@@ -1,28 +1,18 @@
 import { Router } from "express";
 import { models } from "@yahalom-tests/common";
 import { testController } from "../controllers";
-import { validationMiddleware, authMiddleware } from "../middleware";
+import { validationMiddleware } from "../middleware";
 
 export const router = Router();
 
 //Get all tests
-router.get("", authMiddleware, testController.getAllTests);
+router.get("", testController.getAllTests);
 
 //Get tests by id
-router.get("/:id", authMiddleware, testController.getTestById);
+router.get("/:id", testController.getTestById);
 
 //Add test to the list in json
-router.post(
-    "",
-    authMiddleware,
-    validationMiddleware(models.dtos.TestDto),
-    testController.addTest
-);
+router.post("", validationMiddleware(models.dtos.TestDto), testController.addTest);
 
 //Edit test
-router.put(
-    "/:id",
-    authMiddleware,
-    validationMiddleware(models.dtos.TestDto, true),
-    testController.editTest
-);
+router.put("/:id", validationMiddleware(models.dtos.TestDto, true), testController.editTest);
