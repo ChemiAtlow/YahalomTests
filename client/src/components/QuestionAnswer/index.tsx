a085e16955469ab641ac848d3ecc3dc5bd50f5a8
Chemi Atlow <chemiatlow@gmail.com>
2021-01-25 08:57:13 +0200

    Add option to remove answer

--- client/src/components/QuestionAnswer/index.tsx
+++ client/src/components/QuestionAnswer/index.tsx
@@ -1,11 +1,13 @@
 import { models } from "@yahalom-tests/common";
 import React from "react";
 import { FormField } from "../Forms";
+import Icon from "../Icon";
 import "./QuestionAnswer.scoped.scss";
 
 type EditMode = {
     isEditMode: true;
     onContentChange: React.ChangeEventHandler<HTMLInputElement>;
+    onAnswerRemove: React.MouseEventHandler<SVGElement>;
 };
 type ExamMode = {
     isEditMode?: false;
@@ -32,12 +34,10 @@ const QuestionAnswer: React.FC<QuestionAnswerProps> = ({
         questionType === models.enums.QuestionType.MultiChoice ? "checkbox" : "radio";
     return (
         <div className="question__answer">
-            <input
-                className="question__answer-selection"
-                type={selectionType}
-                checked={selected}
-                onChange={onSelectionChange}
-            />
+            <div className="question__answer-selection">
+                <input type={selectionType} checked={selected} onChange={onSelectionChange} />
+                {mode.isEditMode && <Icon icon="close" color="#969696" size={22} onClick={mode.onAnswerRemove} />}
+            </div>
             <div className="question__answer-content">
                 {mode.isEditMode ? (
                     <FormField
