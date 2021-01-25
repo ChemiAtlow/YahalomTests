af87bade77797bfad6f23750bb109c2b5880d9bb
Chemi Atlow <chemiatlow@gmail.com>
2021-01-26 00:07:04 +0200

    Add reset / restore view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,45 @@
 import { BrowserRouter, Switch } from "react-router-dom";
 import { ProvideAuth } from "./hooks/auth.hook";
 import { NavBar, ProtectedRoute, NestedRoutes } from "./components";
-import { Home, Login, Questions, Reports, Tests } from "./views";
+import { Home, Login, Questions, Reports, Restore, Tests } from "./views";
 import { ModalProvider } from "./hooks";
 
 const App: React.FC = () => {
-	return (
-		<ModalProvider>
-			<ProvideAuth>
-				<BrowserRouter>
-					<NavBar />
-					<main className="app">
-						<Switch>
-							<ProtectedRoute
-								requiresField
-								path="/:organizationId/:fieldId">
-								<NestedRoutes>
-									<ProtectedRoute requiresField path="/questions">
-										<Questions />
-									</ProtectedRoute>
-									<ProtectedRoute requiresField path="/tests">
-										<Tests />
-									</ProtectedRoute>
-									<ProtectedRoute requiresField path="/reports">
-										<Reports />
-									</ProtectedRoute>
-								</NestedRoutes>
-							</ProtectedRoute>
-							<ProtectedRoute
-								onlyNonAuth
-								path={["/login", "/signup"]}>
-								<Login />
-							</ProtectedRoute>
-							<ProtectedRoute path="/">
-								<Home />
-							</ProtectedRoute>
-						</Switch>
-					</main>
-				</BrowserRouter>
-			</ProvideAuth>
-		</ModalProvider>
-	);
+    return (
+        <ModalProvider>
+            <ProvideAuth>
+                <BrowserRouter>
+                    <NavBar />
+                    <main className="app">
+                        <Switch>
+                            <ProtectedRoute onlyNonAuth path={["/login", "/signup"]}>
+                                <Login />
+                            </ProtectedRoute>
+                            <ProtectedRoute onlyNonAuth path="/reset/:token?">
+                                <Restore />
+                            </ProtectedRoute>
+                            <ProtectedRoute requiresField path="/:organizationId/:fieldId">
+                                <NestedRoutes>
+                                    <ProtectedRoute requiresField path="/questions">
+                                        <Questions />
+                                    </ProtectedRoute>
+                                    <ProtectedRoute requiresField path="/tests">
+                                        <Tests />
+                                    </ProtectedRoute>
+                                    <ProtectedRoute requiresField path="/reports">
+                                        <Reports />
+                                    </ProtectedRoute>
+                                </NestedRoutes>
+                            </ProtectedRoute>
+                            <ProtectedRoute path="/">
+                                <Home />
+                            </ProtectedRoute>
+                        </Switch>
+                    </main>
+                </BrowserRouter>
+            </ProvideAuth>
+        </ModalProvider>
+    );
 };
 
 export default App;
