2c2baecbab563186ed505611cf45042d189dd419
Chemi Atlow <chemiatlow@gmail.com>
2021-01-25 12:05:22 +0200

    Fix question service

--- server/src/services/question.service.ts
+++ server/src/services/question.service.ts
@@ -1,6 +1,6 @@
 import { models } from "@yahalom-tests/common";
 import { questionRepository } from "../DAL";
-import { BadRequestError, ItemNotInDbError } from "../errors";
+import { ItemNotInDbError } from "../errors";
 import { fieldService, organizationService, testService } from "../services";
 
 // Get Questions
@@ -44,9 +44,13 @@ export const addQuestion = async (
 export const editQuestion = async (
     id: models.classes.guid,
     updatedQuestion: models.dtos.QuestionDto
-) => {
-    await questionRepository.updateItem(id, { ...updatedQuestion, lastUpdate: Date.now() });
-};
+) =>
+    await questionRepository.updateItem(id, {
+        ...updatedQuestion,
+        active: undefined,
+        testCount: undefined,
+        lastUpdate: Date.now(),
+    });
 
 export const isQuestionActive = async (
     questionId: models.classes.guid,
@@ -57,7 +61,5 @@ export const isQuestionActive = async (
     return useCount.length > 0;
 };
 
-export const deleteQuestion = async (id: models.classes.guid) => {
-    const question = await getQuestionById(id);
+export const deleteQuestion = async (id: models.classes.guid) =>
     await questionRepository.deleteItem(id);
-};
