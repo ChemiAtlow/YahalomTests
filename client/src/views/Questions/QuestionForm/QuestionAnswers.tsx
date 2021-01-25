82ce0e86e7337f8312a3a0f72fae28d9b5d72699
Chemi Atlow <chemiatlow@gmail.com>
2021-01-25 09:45:36 +0200

    Add answer validations

--- client/src/views/Questions/QuestionForm/QuestionAnswers.tsx
+++ client/src/views/Questions/QuestionForm/QuestionAnswers.tsx
@@ -1,16 +1,17 @@
 import { models } from "@yahalom-tests/common";
 import React from "react";
-import { Row, QuestionAnswer } from "../../../components";
+import { Row, QuestionAnswer, AppButton } from "../../../components";
 
 
 export type QuestionAnswersKeys = Pick<models.interfaces.Question, "type" | "answers">;
 interface QuestionAnswersProps {
     question: QuestionAnswersKeys;
     onChange: (change: Partial<QuestionAnswersKeys>) => void;
+    onValidityChange: (change: string) => void;
 }
 
-export const QuestionAnswers: React.FC<QuestionAnswersProps> = ({ question, onChange }) => {
-    const onSelectionChanged = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+export const QuestionAnswers: React.FC<QuestionAnswersProps> = ({ question, onChange, onValidityChange }) => {
+    const onCorrectAnswerChanged = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
         //check if question is singlechoice
         if (question.type === models.enums.QuestionType.SingleChoice) {
             question.answers.forEach((answer, i) => {
@@ -20,6 +21,7 @@ export const QuestionAnswers: React.FC<QuestionAnswersProps> = ({ question, onCh
         else {
             question.answers[index].correct = e.target.checked;
         }
+        validate();
         onChange({ answers: question.answers });
     };
     //needs to add new answer to existing question
@@ -27,14 +29,59 @@ export const QuestionAnswers: React.FC<QuestionAnswersProps> = ({ question, onCh
         const { answers } = question;
         const { value } = e.target;
         answers[index].content = value;
-        //check automatocaly add new answer
+        //automatically add new answer
         if (answers.length < 10 && index === answers.length - 1 && value) {
             answers.push({ content: "", correct: false });
-        } else if (index === answers.length - 2 && !value && !answers?.[index + 1]?.content) {
+        }
+        //automatically remove last answer.
+        else if (index === answers.length - 2 && !value && !answers?.[index + 1]?.content) {
             answers.pop();
         }
+        validate();
         onChange({ answers });
     };
+    const onAnswerRemove = (index: number) => {
+        if (question.answers.length === 1) {
+            return;
+        }
+        question.answers.splice(index, 1);
+        validate();
+        onChange({ answers: question.answers })
+    }
+    const onAnswerAdd = () => {
+        if (question.answers.length >= 10) {
+            return;
+        }
+        onChange({ answers: [...question.answers, { content: "", correct: false }] });
+        validate();
+    }
+    const validate = () => {
+        const { answers } = question;
+        const errors: string[] = [];
+        //Check at least 2 questions exist
+        if (answers.length < 2) {
+            errors.push("at least 2 answers are required")
+        }
+        //only last answer can be empty
+        if (answers.some((ans, i) => !ans.content.trim() && i !== answers.length - 1)) {
+            errors.push("Empty answers are invalid");
+        }
+        //empty answer can't be correct
+        if (answers.some(ans => !ans.content.trim() && ans.correct)) {
+            errors.push("Empty answer can't be correct")
+        }
+        //no answer is marked as correct
+        if (!answers.some(ans => ans.correct)) {
+            errors.push("At least one question must be correct")
+        }
+        let errMsg = "";
+        if (errors.length === 1) {
+            errMsg = `Error: ${errors[0]}`
+        } else if (errors.length > 1) {
+            errMsg = `Errors: ${errors.join(", ")}`
+        }
+        onValidityChange(errMsg)
+    }
     return (
         <div className="container">
             <Row>
@@ -47,11 +94,13 @@ export const QuestionAnswers: React.FC<QuestionAnswersProps> = ({ question, onCh
                         mode={{
                             isEditMode: true,
                             onContentChange: e => onContentChange(e, i),
+                            onAnswerRemove: () => onAnswerRemove(i)
                         }}
                         selected={correct}
-                        onSelectionChange={e => onSelectionChanged(e, i)}
+                        onSelectionChange={e => onCorrectAnswerChanged(e, i)}
                     />
                 ))}
+                {question.answers.length < 10 && <AppButton type="button" onClick={onAnswerAdd}>Add answer</AppButton>}
             </Row>
         </div>
     );
