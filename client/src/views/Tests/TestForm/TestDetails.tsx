e91b71300795aa5e36d4345707935fa00baecfd6
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-26 17:01:38 +0200

    Fixed callbacks and validity

--- client/src/views/Tests/TestForm/TestDetails.tsx
+++ client/src/views/Tests/TestForm/TestDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { models } from '@yahalom-tests/common';
 import { Row, FormField, Select } from '../../../components';
 import { useAuth } from "../../../hooks";
@@ -6,7 +6,7 @@ import { enumToArray, SwitchCamelCaseToHuman } from '../../../utils';
 
 const languages = enumToArray(models.enums.Language).map(SwitchCamelCaseToHuman);
 export type TestDetailsKeys = Pick<models.dtos.TestDto,
-    "language" | "intro" | "minPassGrade" | "title">;
+    "language" | "intro" | "minPassGrade" | "title" | "isReviewEnabled">;
 
 interface TestDetailsProps {
     test: TestDetailsKeys;
@@ -16,12 +16,58 @@ interface TestDetailsProps {
 
 export const TestDetails: React.FC<TestDetailsProps> = ({ test, onValidityChange, onChange }) => {
     const { activeStudyField } = useAuth();
+    const [titleError, setTitleError] = useState("");
+    const [gradeError, setGradeError] = useState("");
+    const [introError, setIntroError] = useState("");
 
-    const onLanguageSelected = (e: React.ChangeEvent<HTMLSelectElement>) => { };
-    const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) => { };
-    const onPassingGradeChange = (e: React.ChangeEvent<HTMLInputElement>) => { };
-    const onReviewedChanged = (e: React.ChangeEvent<HTMLInputElement>) => { };
-    const onIntroChanged = (e: React.ChangeEvent<HTMLInputElement>) => { };
+    useEffect(() => {
+        let errorStr = "";
+        const errors = [titleError, introError, gradeError];
+        if (titleError || introError || gradeError) {
+            if (titleError && introError && gradeError) {
+                errorStr = `Errors: ${titleError}, ${introError},${gradeError}`;
+            } else {
+                errorStr = `Errors: `;
+                errors.forEach(err => {
+                    if (err) { errorStr += `${err},`; }
+                });
+                errorStr.slice(0, -1);
+            }
+        }
+        onValidityChange(errorStr);
+    }, [titleError, introError,gradeError, onValidityChange]);
+
+    const onLanguageSelected = (e: React.ChangeEvent<HTMLSelectElement>) => { onChange({ language: e.target.selectedIndex - 1 }); };
+
+    const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        stringPropsErrorValidate(value, "title");
+        onChange({ title: value });
+    };
+
+    const onPassingGradeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setGradeError("");
+        const grade = +e.target.value;
+        //check if grade isnt between 1-99
+        if (!(grade > 1) || !(grade < 99)) {
+            setGradeError("Passing grade must be between 1-99 ");
+        }
+        onChange({ minPassGrade: e.target.value as unknown as number });
+    };
+    const onReviewedChanged = (e: React.ChangeEvent<HTMLInputElement>) => { onChange({ isReviewEnabled: e.target.checked }) };
+
+    const onIntroChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        stringPropsErrorValidate(value, "intro");
+        onChange({ intro: e.target.value });
+    };
+
+    const stringPropsErrorValidate = (value: string, propName: string) => {
+        propName === "Intro" ? setIntroError("") : setTitleError("");
+        if (!value.trim()) {
+            propName === "intro" ? setIntroError(`${propName} is required!`) : setTitleError(`${propName} is required!`);
+        }
+    };
 
     return (
         <div>
@@ -38,6 +84,7 @@ export const TestDetails: React.FC<TestDetailsProps> = ({ test, onValidityChange
                         required
                         value={test.title}
                         onChange={onTitleChanged}
+                        error={titleError}
                     />
                     <FormField label="Passing grade"
                         type="number"
@@ -46,16 +93,18 @@ export const TestDetails: React.FC<TestDetailsProps> = ({ test, onValidityChange
                         required
                         value={test.minPassGrade}
                         onChange={onPassingGradeChange}
+                        error={gradeError}
                     />
                     <div>
                         <label>Show correct answers after submission</label>
-                        <input type="checkbox" value="Review" onChange={onReviewedChanged} />
+                        <input type="checkbox" onChange={onReviewedChanged} />
                     </div>
                     <FormField label="Test intro - Header"
                         type="textarea"
                         required
                         value={test.intro}
                         onChange={onIntroChanged}
+                        error={introError}
                     />
                 </Row>
             </div>
