f5fa3779dfffd473d54f5f3ab7f383a3472e65c5
Chemi Atlow <chemiatlow@gmail.com>
2021-01-26 09:00:34 +0200

    Create models

---
+++ common/src/models/interfaces/Exam.ts
@@ -0,0 +1,13 @@
+import { guid } from "../classes";
+import { Language } from "../enums";
+import { AnsweredQuestion } from "./AnsweredQuestion";
+
+export interface Exam {
+    id: guid;
+    test: guid;
+    language: Language;
+    title: string;
+    intro: string;
+    student: string; //the email of student, which is the unique key
+    questions: AnsweredQuestion[];
+}
