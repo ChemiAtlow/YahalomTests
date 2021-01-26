630234258599e037fa8ea9ae503bed73b5d728a5
Chemi Atlow <chemiatlow@gmail.com>
2021-01-26 09:25:25 +0200

    naming convention

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,4 +1,4 @@
-export { router as questionsRouter } from "./question.routes";
-export { router as authRouter } from "./auth.routes";
+export { router as questionsRoutes } from "./question.routes";
+export { router as authRoutes } from "./auth.routes";
 export { router as testRoutes } from "./test.routes";
 export { router as reportRoutes } from "./report.routes";
