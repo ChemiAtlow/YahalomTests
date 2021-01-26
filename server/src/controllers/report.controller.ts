955f6519976393167772f24f1430fe58b5ea91f6
Chemi Atlow <chemiatlow@gmail.com>
2021-01-26 09:26:24 +0200

    Fix import + lint

--- server/src/controllers/report.controller.ts
+++ server/src/controllers/report.controller.ts
@@ -1,13 +1,13 @@
 import { Response } from "express";
-import { types } from "server/dist/models";
 import { HTTPStatuses } from "../constants";
+import { types } from "../models";
 
 export const getTestReport = (req: types.RequestWithId, res: Response) => {
     //Technicaly this should be 501 - not implemented, but this will work...
     res.sendStatus(HTTPStatuses.iAmATeapot);
-}
+};
 
 export const getStudentReport = (req: types.RequestWithId, res: Response) => {
     //Technicaly this should be 501 - not implemented, but this will work...
     res.sendStatus(HTTPStatuses.iAmATeapot);
-}
+};
