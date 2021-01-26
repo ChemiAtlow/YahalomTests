314c4343bb5c22b642efbdc6e6610582864271a6
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-26 17:02:37 +0200

    Fixed callbacks and emailForm

--- client/src/views/Tests/EditTest.tsx
+++ client/src/views/Tests/EditTest.tsx
@@ -1,6 +1,6 @@
 import { models } from '@yahalom-tests/common';
 import React, { useState } from 'react'
-import { SectionNavigator, Section } from '../../components'
+import { SectionNavigator, Section, AppButton } from '../../components'
 import { TestDetails, TestEmails, TestQuestions } from "./TestForm";
 
 const EditTest: React.FC = () => {
@@ -17,37 +17,38 @@ const EditTest: React.FC = () => {
         successEmail: { body: "", subject: "" },
         failureEmail: { body: "", subject: "" },
     });
-    //#region test details callbacks
+
+    const [detailsError, setDetailsError] = useState("");
+    const [emailsError, setEmailsError] = useState("");
+    const [questionsError, setQuestionsError] = useState("");
+    const isInvalid = Boolean(emailsError) || Boolean(questionsError) || Boolean(detailsError);
+
     const detailsValidityChanged = () => { };
-    const onDetailsChange = (changed: Partial<models.dtos.TestDto>) => { };
-    //#endregion
-
-    //#region test questions callbacks
-    const onQuestionsChange = (changed: Partial<models.dtos.TestDto>) => {
+    const onChange = (changed: Partial<models.dtos.TestDto>) => {
+        console.log(test);
         setTest({ ...test, ...changed });
-        console.log(test.questions); //temporary - verify test contains the questions.
     };
-    const questionsValidityChange = () => { };
-    //#endregion
 
-    //#region test emails callbacks
+    const questionsValidityChange = () => { };
+
     const emailsValidityChanged = () => { };
-    const onEmailsChange = (changed: Partial<models.dtos.TestDto>) => { };
-    //#endregion
 
     return (
         <form>
             <SectionNavigator>
-                <Section label="Test Details">
-                    <TestDetails test={test} onChange={e => onDetailsChange(e)} onValidityChange={detailsValidityChanged} />
+                <Section label="Test Details" errMsg={detailsError}>
+                    <TestDetails test={test} onChange={e => onChange(e)} onValidityChange={detailsValidityChanged} />
                 </Section>
-                <Section label="Test Complition">
-                    <TestEmails test={test} onChange={e => onEmailsChange(e)} onValidityChange={emailsValidityChanged} />
+                <Section label="Test Complition" errMsg={emailsError}>
+                    <TestEmails test={test} onChange={e => onChange(e)} onValidityChange={emailsValidityChanged} />
                 </Section>
-                <Section label="Test Questions">
-                    <TestQuestions test={test} onChange={e => onQuestionsChange(e)} onValidityChange={questionsValidityChange} />
+                <Section label="Test Questions" errMsg={questionsError}>
+                    <TestQuestions test={test} onChange={e => onChange(e)} onValidityChange={questionsValidityChange} />
                 </Section>
             </SectionNavigator>
+            <AppButton disabled={!isInvalid} type="submit" className="edit-question__form">
+                {test.id ? "Edit" : "Create"}
+            </AppButton>
         </form>
     )
 }
--- client/src/views/Tests/TestForm/TestEmails.tsx
+++ client/src/views/Tests/TestForm/TestEmails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { models } from '@yahalom-tests/common';
 import { FormField } from '../../../components';
 import { EmailForm } from "./EmailForm"
@@ -13,10 +13,39 @@ interface TestEmailsProps {
 };
 
 export const TestEmails: React.FC<TestEmailsProps> = ({ test, onChange, onValidityChange }) => {
-    const onSuccessMessageChanged = (e: React.ChangeEvent<HTMLInputElement>) => { };
-    const onSuccessMailChange = (e: React.ChangeEvent<models.dtos.EmailDto>) => { };
-    const onFailureMessageChanged = (e: React.ChangeEvent<HTMLInputElement>) => { };
-    const onFailureMailChange = (e: React.ChangeEvent<models.dtos.EmailDto>) => { };
+    const [successMsgError, setSuccessMsgError] = useState("");
+    const [failureMsgError, setFailureMsgError] = useState("");
+    const [successBodyError, setSuccessBodyError] = useState("");
+    const [failureBodyError, setFaliureBodyError] = useState("");
+
+    const successEmailValidity = () => { };
+    const failureEmailValidity = () => { };
+
+    const onSuccessMessageChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        stringPropsErrorValidate(value, "Success message");
+        onChange({ successMessage: value });
+    };
+
+    const onFailureMessageChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        stringPropsErrorValidate(value, "Failure message");
+        onChange({ failureMessage: value });
+    };
+
+    const onSuccessMailChange = (changed: Partial<models.dtos.EmailDto>) => { };
+
+    const onFailureMailChange = (changed: Partial<models.dtos.EmailDto>) => {
+        onChange({ failureEmail: changed.subject as string, failureMessage: changed.body });
+        console.log(changed);
+    };
+
+    const stringPropsErrorValidate = (value: string, propName: string) => {
+        propName === "Success message" ? setSuccessMsgError("") : setFailureMsgError("");
+        if (!value.trim()) {
+            propName === "Success message" ? setSuccessMsgError(`${propName} is required!`) : setFailureMsgError(`${propName} is required!`);
+        }
+    };
 
     return (
         <>
@@ -25,15 +54,17 @@ export const TestEmails: React.FC<TestEmailsProps> = ({ test, onChange, onValidi
                 required
                 value={test.successMessage}
                 onChange={onSuccessMessageChanged}
+                error={successMsgError}
             />
-            <EmailForm email={test.successEmail} onChange={onSuccessMailChange} />
+            <EmailForm email={test.successEmail} onChange={onSuccessMailChange} onValidityChange={successEmailValidity} />
             <FormField label="Failure message"
                 type="textarea"
                 required
                 value={test.failureMessage}
                 onChange={onFailureMessageChanged}
+                error={failureMsgError}
             />
-            <EmailForm email={test.successEmail} onChange={onFailureMailChange} />
+            <EmailForm email={test.successEmail} onChange={onFailureMailChange} onValidityChange={failureEmailValidity} />
         </>
     )
 }
