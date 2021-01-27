8a7430a93acd4b923ccada09cb3a7b75504ee07d
Chemi Atlow <chemiatlow@gmail.com>
2021-01-27 09:31:47 +0200

    avoid selecting form-field label

--- client/src/components/Forms/FormField.tsx
+++ client/src/components/Forms/FormField.tsx
@@ -1,44 +1,39 @@
 import React from "react";
 import "./FormField.scoped.scss";
 
-interface FormFieldProps
-	extends React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
-	type: "text" | "password" | "number" | "textarea" | "radio" | "checkbox";
-	label: string;
-	error?: string;
+interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
+    type: "text" | "password" | "number" | "textarea" | "radio" | "checkbox";
+    label: string;
+    error?: string;
 }
-const FormField: React.FC<FormFieldProps> = ({
-	label,
-	error,
-	type,
-	required,
-	...rest
-}) => {
-	return (
-		<div className={`form-field ${error ? "error" : ""}`}>
-			<label className="form-field__control">
-				{type === "textarea" ? (
-					<textarea
-						className="form-field__control-input"
-						placeholder=" "
-						required
-						{...rest}
-					/>
-				) : (
-					<input
-						className="form-field__control-input"
-						placeholder=" "
-						required
-						type={type}
-						{...rest}
-					/>
-				)}
-				<span className="form-field__control-label">{`${label}${required ? '*' : ''}`}</span>
-				<div className="form-field__control-bar" />
-			</label>
-			{error && <p className="form-field__error">{error}</p>}
-		</div>
-	);
+const FormField: React.FC<FormFieldProps> = ({ label, error, type, required, ...rest }) => {
+    return (
+        <div className={`form-field ${error ? "error" : ""}`}>
+            <label className="form-field__control">
+                {type === "textarea" ? (
+                    <textarea
+                        className="form-field__control-input"
+                        placeholder=" "
+                        required
+                        {...rest}
+                    />
+                ) : (
+                    <input
+                        className="form-field__control-input"
+                        placeholder=" "
+                        required
+                        type={type}
+                        {...rest}
+                    />
+                )}
+                <span className="form-field__control-label">{`${label}${
+                    required ? "*" : ""
+                }`}</span>
+                <div className="form-field__control-bar" />
+            </label>
+            {error && <p className="form-field__error">{error}</p>}
+        </div>
+    );
 };
 
 export default FormField;
