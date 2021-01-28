7146182b1b945ad81651a25f3c918ebad7f7bbfc
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-28 03:47:16 +0200

    Add helper method

--- server/src/services/organization.service.ts
+++ server/src/services/organization.service.ts
@@ -1,6 +1,8 @@
 import { models } from "@yahalom-tests/common";
+import { fieldService } from ".";
+import { HTTPStatuses } from "../constants";
 import { organizationRepository } from "../DAL";
-import { ItemNotInDbError } from "../errors";
+import { HttpError, ItemNotInDbError } from "../errors";
 
 export const getAllOrganizations = async () => {
     return await organizationRepository.getAll();
@@ -61,4 +63,16 @@ export const isQuestionConnectedToOrganization = async (
 ) => {
     const organization = await getOrganizationById(organizationId);
     return organization.questions.includes(questionId);
+};
+
+export const getOrganizationByTestId = async (testId: models.classes.guid) => {
+    const organizations = await getAllOrganizations();
+    for await (const org of organizations) { //for on organizations
+        for await (const fieldId of org.fields) { //for on fields in organization
+            const field = await fieldService.getStudyFieldById(fieldId);
+            if (field.tests.includes(testId)) { return org; }
+        }
+    }
+    //testId doesnt exist
+    throw new HttpError(HTTPStatuses.notFound, "No organization contain this test!");
 };
