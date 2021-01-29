92f223eee661e594f17d0f15d5b8063a9536dec8
Chemi Atlow <chemiatlow@gmail.com>
2021-01-29 14:06:47 +0200

    add all routes to exam service

--- client/src/services/examService.ts
+++ client/src/services/examService.ts
@@ -10,3 +10,15 @@ export async function checkIfTestIdIsValid(testId: models.classes.guid) {
 export async function requestToStartExam(testId: models.classes.guid, student: models.dtos.StudentDto) {
     return await http.post<models.interfaces.Exam>(`${examRoute}new/${testId}`, student);
 };
+
+export async function getExam(examId: models.classes.guid) {
+    return await http.get<models.interfaces.Exam>(`${examRoute}${examId}`);
+};
+
+export async function updateExam(examId: models.classes.guid, update: models.dtos.ExamChangeDto) {
+    return await http.put<void>(`${examRoute}${examId}`, update);
+};
+
+export async function submitExam(examId: models.classes.guid) {
+    return await http.get<void>(`${examRoute}${examId}/submit`);
+};
