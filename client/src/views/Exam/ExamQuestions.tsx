256600c4e21bfa9cb3a46ddd2988025cde5ff9d2
Chemi Atlow <chemiatlow@gmail.com>
2021-01-30 20:27:05 +0200

    handle page number + check if we are handleing an exam result

--- client/src/views/Exam/ExamQuestions.tsx
+++ client/src/views/Exam/ExamQuestions.tsx
@@ -1,18 +1,20 @@
 import { models } from '@yahalom-tests/common';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { match, useLocation } from 'react-router-dom';
 import { useLoading } from '../../hooks';
 import { examService } from '../../services';
 
 interface ExamProps {
-    match: match<{ examId: models.classes.guid }>;
+    match: match<{ examId: models.classes.guid; page?: string }>;
 }
 
 const ExamQuestions: React.FC<ExamProps> = ({ match }) => {
-    const { examId } = match.params;
+    const { examId, page } = match.params;
+    const pageNumber = isNaN(Number(page)) ? -1 : Number(page);
     const { state } = useLocation<{ exam?: models.interfaces.Exam }>();
-    const [exam, setExam] = useState<models.interfaces.Exam>();
+    const [exam, setExam] = useState<models.interfaces.Exam | models.interfaces.ExamResult>();
     const { setLoadingState } = useLoading();
+    const isExamResult = useCallback((exam: any): exam is models.interfaces.ExamResult => exam.hasOwnProperty('originalQuestions'), []);
     useEffect(() => {
         if (examId && state?.exam) {
             setExam(state.exam);
@@ -26,13 +28,16 @@ const ExamQuestions: React.FC<ExamProps> = ({ match }) => {
                 }).catch(err => setLoadingState("failure", `An error occoured while loading the exam:\n${err?.message || ""}`))
         }
     }, [state, examId, setExam, setLoadingState])
-
-    return (
-        <div>
-            {exam?.student}
-            {examId}
-        </div>
-    )
+    if (isExamResult(exam)) {
+        return <p>{exam.message}</p>
+    } else {
+        return pageNumber < 0 ?
+            <>
+            <h1>{exam?.title}</h1>
+            <p>{exam?.intro}</p>
+            </>
+            : <p>{exam?.questions[pageNumber].title}</p>
+    }
 }
 
 export default ExamQuestions
