29f568fc567d5776a3d30797813f68e2933bb11c
Chemi Atlow <chemiatlow@gmail.com>
2021-01-30 19:32:50 +0200

    Simplify and extract logic

--- client/src/views/Exam/index.tsx
+++ client/src/views/Exam/index.tsx
@@ -1,44 +1,16 @@
-import { models } from '@yahalom-tests/common';
-import React, { useEffect } from 'react';
-import { match, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { Container } from '../../components';
-import { useLoading } from '../../hooks';
-import { examService } from '../../services';
 import ExamQuestions from './ExamQuestions';
 import StudentForm from './StudentForm';
 
-interface ExamProps {
-    match: match<{ testId: models.classes.guid }>;
-}
+const Exam: React.FC = () => {
+    const { path } = useRouteMatch();
 
-const Exam: React.FC<ExamProps> = ({ match }) => {
-    const { testId } = match.params;
-    const { path, url } = useRouteMatch();
-    const { push } = useHistory();
-    const { setLoadingState } = useLoading();
-
-    useEffect(() => setLoadingState("loading"), [])
-
-    const onRequestNewExam = async (student: models.dtos.StudentDto) => {
-        console.log(student);
-        setLoadingState("loading");
-        try {
-            const { data } = await examService.requestToStartExam(testId, student);
-            console.log(data);
-            setLoadingState("success");
-            push(`${url}/${data.id}`, { exam: data });
-
-        } catch (error) {
-            console.log(error);
-            setLoadingState("failure", "Couldn't currently start a new exam!");
-        }
-    }
     return (
         <Container>
             <Switch>
-                <Route exact path={path}>
-                    <StudentForm testId={match.params.testId} onRequestNewExam={onRequestNewExam} />
-                </Route>
+                <Route exact path={path} component={StudentForm} />
                 <Route path={`${path}/:examId`} component={ExamQuestions} />
             </Switch>
         </Container>
