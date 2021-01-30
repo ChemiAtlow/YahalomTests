16bf0e79ee020509c9256c75e1d08c433106a2e9
reem.cohen39@gmail.com <reem.cohen39@gmail.com>
2021-01-31 01:23:07 +0200

    Span styling

--- client/src/components/Pagination/index.tsx
+++ client/src/components/Pagination/index.tsx
@@ -11,7 +11,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, maximalPage, onCli
     const prevPage = useMemo(() => currentPage - 1, [currentPage]);
     const nextPage = useMemo(() => currentPage + 1, [currentPage]);
     return (
-        <div>
+        <span className="pagination-wrapper">
             {currentPage > 2 && <AppButton color="gray" varaiety="small" onClick={() => onClick(1)}>1</AppButton>}
             {currentPage > 3 && <AppButton color="gray" varaiety="small" disabled>...</AppButton>}
             {currentPage > 1 && <AppButton color="gray" varaiety="small" onClick={() => onClick(prevPage)}>{prevPage}</AppButton>}
@@ -19,7 +19,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, maximalPage, onCli
             {currentPage < maximalPage && <AppButton color="gray" varaiety="small" onClick={() => onClick(nextPage)}>{nextPage}</AppButton>}
             {currentPage < maximalPage - 2 && <AppButton color="gray" varaiety="small" disabled>...</AppButton>}
             {currentPage < maximalPage - 1 && <AppButton color="gray" varaiety="small" onClick={() => onClick(maximalPage)}>{maximalPage}</AppButton>}
-        </div>
+        </span>
     );
 };
 
