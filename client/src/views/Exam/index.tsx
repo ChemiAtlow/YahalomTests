c9f73d3b46e48605d3fc6a91896b37e62ea72508
Chemi Atlow <chemiatlow@gmail.com>
2021-01-31 12:50:08 +0200

    Rename examQuestions -> examProgress

--- client/src/views/Exam/index.tsx
+++ client/src/views/Exam/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import ExamQuestions from './ExamQuestions';
+import ExamProgress from './ExamProgress';
 import StudentForm from './StudentForm';
 
 const Exam: React.FC = () => {
@@ -9,7 +9,7 @@ const Exam: React.FC = () => {
     return (
         <Switch>
             <Route exact path={path} component={StudentForm} />
-            <Route path={`${path}/:examId/:page?`} component={ExamQuestions} />
+            <Route path={`${path}/:examId/:page?`} component={ExamProgress} />
         </Switch>
     )
 }
