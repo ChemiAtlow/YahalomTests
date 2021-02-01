a62b2e1b3bec5e7065cc8bad60530b23741e537a
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-02-01 10:34:35 +0200

    Drop self handling

--- client/src/views/Tests/TestForm/EmailForm.tsx
+++ client/src/views/Tests/TestForm/EmailForm.tsx
@@ -1,46 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { models } from '@yahalom-tests/common';
 import { FormField } from '../../../components';
+import { TestEmailError } from './types';
 
 interface EmailFormProps {
     email: models.dtos.EmailDto;
+    errors: TestEmailError;
     onChange: (change: Partial<models.dtos.EmailDto>) => void;
-    onValidityChange: (change: string) => void;
 }
 
-export const EmailForm: React.FC<EmailFormProps> = ({ email, onChange, onValidityChange }) => {
-    const [subjectError, setSubjectError] = useState("");
-    const [bodyError, setBodyError] = useState("");
-
-    useEffect(() => {
-        let errorStr = "";
-        if (subjectError || bodyError) {
-            if (subjectError && bodyError) {
-                errorStr = `Errors: ${subjectError}, ${bodyError}`;
-            } else {
-                errorStr = `Error: ${subjectError || bodyError}`;
-            }
-        }
-        onValidityChange(errorStr);
-    }, [subjectError, bodyError, onValidityChange]);
-
-    const stringPropsErrorValidate = (value: string, propName: string) => {
-        propName === "subject" ? setSubjectError("") : setBodyError("");
-        if (!value.trim()) {
-            propName === "subject" ? setSubjectError(`${propName} is required!`) : setBodyError(`${propName} is required!`);
-        }
-    };
-
-    const onSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        stringPropsErrorValidate(value, "subject");
-        onChange({ subject: value });
-    };
-    const onBodyChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        stringPropsErrorValidate(value, "body");
-        onChange({ body: value });
-    };
+export const EmailForm: React.FC<EmailFormProps> = ({ email, onChange, errors }) => {
+    const genericChange = <K extends keyof models.dtos.EmailDto, V extends models.dtos.EmailDto[K]>(key: K, value: V) => {
+        onChange({ [key]: value })
+    }
 
     return (
         <>
@@ -48,15 +20,15 @@ export const EmailForm: React.FC<EmailFormProps> = ({ email, onChange, onValidit
                 type="text"
                 required
                 value={email?.subject}
-                onChange={onSubjectChange}
-                error={subjectError}
+                onChange={({target}) => genericChange("subject", target.value)}
+                error={errors.subject}
             />
             <FormField label="Mail body"
                 type="textarea"
                 required
                 value={email?.body}
-                onChange={onBodyChanged}
-                error={bodyError}
+                onChange={({target}) => genericChange("body", target.value)}
+                error={errors.body}
             />
         </>
     )
