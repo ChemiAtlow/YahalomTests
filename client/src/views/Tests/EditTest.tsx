772f7a7c58df664bfae08911fbae17b0fb32b507
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-02-01 17:35:32 +0200

    Forms work (#85)
    * Add template example
    
    * move validators of details and questions to test
    
    * Rename view
    
    * drop redundant type
    
    * extract types
    
    * Add error types
    
    * Switch to simpler error
    
    * Drop self handling
    
    * stop self handling
    
    * Handle all child forms
    
    * Fix formfield size
    
    * drop redundant types
    
    * Use import type
    
    * Question form self validations

--- client/src/views/Tests/EditTest.tsx
+++ client/src/views/Tests/EditTest.tsx
@@ -1,10 +1,11 @@
 import { models } from '@yahalom-tests/common';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useLocation, useRouteMatch } from 'react-router-dom';
 import { SectionNavigator, Section, AppButton, ErrorModal, MessageModal, FixedFooter } from '../../components'
 import { useAuth, useModal } from '../../hooks';
 import { testService } from '../../services';
-import { TestDetails, TestEmails, TestQuestions } from "./TestForm";
+import { TestDetails, TestMessages, TestQuestions } from "./TestForm";
+import type { TestDetailsKeys, TestMessagesKeys, TestDetailsError, TestMessagesError  } from './TestForm/types';
 
 interface EditTestProps {
     onTestAddedOrEdited: (test: models.interfaces.Test) => void;
@@ -28,24 +29,96 @@ const EditTest: React.FC<EditTestProps> = ({ onTestAddedOrEdited }) => {
     });
     const { openModal } = useModal();
     const { buildAuthRequestData } = useAuth();
-    const [detailsError, setDetailsError] = useState("");
-    const [emailsError, setEmailsError] = useState("");
+    const [detailsErrors, setDetailsErrors] = useState<TestDetailsError>({
+        general: "",
+        intro: "",
+        minPassGrade: "",
+        title: ""
+    });
+    const [messagesErrors, setMessagesErrors] = useState<TestMessagesError>({
+        general: "",
+        successMessage: "",
+        failureMessage: "",
+        failureEmail: {
+            body: "",
+            subject: ""
+        },
+        successEmail: {
+            body: "",
+            subject: ""
+        }
+    });
     const [questionsError, setQuestionsError] = useState("");
     const { params } = useRouteMatch<EditParams>();
     const { state } = useLocation<{ test?: models.dtos.TestDto }>();
+    const isDetailsPageInvalid = useMemo(
+        () => !test.title.trim() || !test.intro.trim() || test.minPassGrade < 1 || test.minPassGrade > 100,
+        [test.title, test.intro, test.minPassGrade]
+    );
+    const isMessagesPageInvalid = useMemo(
+        () => !test.successMessage.trim() || !test.successEmail.body.trim() || !test.successEmail.subject.trim() ||
+            !test.failureMessage.trim() || !test.failureEmail.body.trim() || !test.failureEmail.subject.trim(),
+        [test.successMessage, test.successEmail, test.failureMessage, test.failureEmail]
+    );
+    const isQuestionsPageInvalid = useMemo(() => test.questions.length <= 0, [test]);
     const isInvalid =
         //check for errors from components
-        Boolean(emailsError) || Boolean(questionsError) || Boolean(detailsError) ||
+        Boolean(messagesErrors.general) || Boolean(questionsError) || Boolean(detailsErrors.general) ||
         //check validate in case the user didn't write and make a errorChange.
-        !Boolean(test.title)
-        || !Boolean(test.successMessage)
-        || !Boolean(test.successEmail) || !Boolean(test.questions?.length > 0) || !Boolean(test.minPassGrade)
-        || !Boolean(test.intro) || !Boolean(test.failureMessage) || !Boolean(test.failureEmail);
+        isDetailsPageInvalid || isMessagesPageInvalid || isQuestionsPageInvalid;
 
     const onChange = (changed: Partial<models.dtos.TestDto>) => {
         setTest({ ...test, ...changed });
-        console.log({ ...test, ...changed });
     };
+    const onChangeDetails = (changed: Partial<TestDetailsKeys>) => {
+        if (changed.title !== undefined) {
+            const title = changed.title.trim() ? "" : "Title is required!";
+            setDetailsErrors({ ...detailsErrors, title });
+        }
+        if (changed.intro !== undefined) {
+            const intro = changed.intro.trim() ? "" : "Intro is required!";
+            setDetailsErrors({ ...detailsErrors, intro });
+        }
+        if (changed.minPassGrade !== undefined) {
+            const minPassGrade = changed.minPassGrade < 1 ? "Minimal passing grade is 1!" :
+                changed.minPassGrade >= 100 ? "Minimal passing grade can't be 100 or more!" : "";
+            setDetailsErrors({ ...detailsErrors, minPassGrade });
+        }
+        onChange(changed);
+    }
+    const onChangeEmails = (changed: Partial<TestMessagesKeys>) => {
+        if (changed.failureMessage !== undefined) {
+            const failureMessage = changed.failureMessage.trim() ? "" : "Message is required!";
+            setMessagesErrors({ ...messagesErrors, failureMessage });
+        }
+        if (changed.successMessage !== undefined) {
+            const successMessage = changed.successMessage.trim() ? "" : "Message is required!";
+            setMessagesErrors({ ...messagesErrors, successMessage });
+        }
+        if (changed.failureEmail !== undefined) {
+            if (changed.failureEmail.subject !== undefined) {
+                const failureEmailSubject = changed.failureEmail.subject.trim() ? "" : "Email subject is required!";
+                setMessagesErrors({ ...messagesErrors, failureEmail: { ...messagesErrors.failureEmail, subject: failureEmailSubject } });
+            }
+            if (changed.failureEmail.body !== undefined) {
+                const failureEmailBody = changed.failureEmail.body.trim() ? "" : "Email body is required!";
+                setMessagesErrors({ ...messagesErrors, failureEmail: { ...messagesErrors.failureEmail, body: failureEmailBody } });
+            }
+            changed.failureEmail = { ...test.failureEmail, ...changed.failureEmail };
+        }
+        if (changed.successEmail !== undefined) {
+            if (changed.successEmail.subject !== undefined) {
+                const successEmailSubject = changed.successEmail.subject.trim() ? "" : "Email subject is required!";
+                setMessagesErrors({ ...messagesErrors, successEmail: { ...messagesErrors.successEmail, subject: successEmailSubject } });
+            }
+            if (changed.successEmail.body !== undefined) {
+                const successEmailBody = changed.successEmail.body.trim() ? "" : "Email body is required!";
+                setMessagesErrors({ ...messagesErrors, successEmail: { ...messagesErrors.successEmail, body: successEmailBody } });
+            }
+            changed.successEmail = { ...test.successEmail, ...changed.successEmail };
+        }
+        onChange(changed);
+    }
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (isInvalid) {
@@ -83,6 +156,13 @@ const EditTest: React.FC<EditTestProps> = ({ onTestAddedOrEdited }) => {
         };
         return testClone;
     };
+    useEffect(() => {
+        const detailsError = isDetailsPageInvalid ? "There are missing details in the form!" : "";
+        setDetailsErrors(detailsErrors => ({ ...detailsErrors, general: detailsError }));
+        const messagesError = isMessagesPageInvalid ? "There are missing details in the form!" : "";
+        setMessagesErrors(messagesErrors => ({ ...messagesErrors, general: messagesError }));
+        setQuestionsError(isQuestionsPageInvalid ? "Please select at least one question!" : "");
+    }, [setQuestionsError, isDetailsPageInvalid, isMessagesPageInvalid, isQuestionsPageInvalid])
 
     useEffect(() => {
         if (params.testId && state?.test) {
@@ -99,18 +179,18 @@ const EditTest: React.FC<EditTestProps> = ({ onTestAddedOrEdited }) => {
 
     return (
         <FixedFooter>
-        <form onSubmit={onSubmit} noValidate id="edit-test__form">
-            <SectionNavigator>
-                <Section label="Test Details" errMsg={detailsError} isValid={!detailsError}>
-                    <TestDetails test={test} onChange={onChange} onValidityChange={setDetailsError} />
-                </Section>
-                <Section label="Completion messages" errMsg={emailsError} isValid={!emailsError}>
-                    <TestEmails test={test} onChange={onChange} onValidityChange={setEmailsError} />
-                </Section>
-                <Section label="Test Questions" errMsg={questionsError} isValid={!questionsError}>
-                    <TestQuestions test={test} onChange={onChange} onValidityChange={setQuestionsError} />
-                </Section>
-            </SectionNavigator>
+            <form onSubmit={onSubmit} noValidate id="edit-test__form">
+                <SectionNavigator>
+                    <Section label="Test Details" errMsg={detailsErrors.general} isValid={!detailsErrors.general}>
+                        <TestDetails test={test} onChange={onChangeDetails} errors={detailsErrors} />
+                    </Section>
+                    <Section label="Completion messages" errMsg={messagesErrors.general} isValid={!messagesErrors.general}>
+                        <TestMessages test={test} onChange={onChangeEmails} errors={messagesErrors} />
+                    </Section>
+                    <Section label="Test Questions" errMsg={questionsError} isValid={!questionsError}>
+                        <TestQuestions test={test} onChange={onChange} />
+                    </Section>
+                </SectionNavigator>
             </form>
             <div>
                 <AppButton disabled={isInvalid} type="submit" form="edit-test__form">
---
+++ client/src/views/Tests/TestForm/TestMessages.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+    Accordion,
+    AccordionSection,
+    Column,
+    Container,
+    DataTable,
+    FormField,
+} from '../../../components';
+import { EmailForm } from "./EmailForm";
+import type { TestMessagesError, TestMessagesKeys } from './types';
+
+const columns: Column[] = [
+    {
+        label: 'Template',
+        key: 'key',
+        sortable: true,
+    },
+    {
+        label: 'Value',
+        key: 'value',
+        sortable: true,
+        largeColumn: true,
+    },
+];
+const emailTemplates = [
+    { key: '@TestName@', value: 'The name of the test.' },
+    { key: '@FirstName@', value: 'First name of student who took the exam.' },
+    { key: '@LastName@', value: 'Last name of student who took the exam.' },
+    { key: '@Date@', value: 'Date of exam completion.' },
+    { key: '@Grade@', value: 'The grade the student has gotten.' },
+    {
+        key: '@Certificate@',
+        value: 'A link to the course certificate - will work only for students who passed.',
+    },
+];
+
+interface TestEmailsProps {
+    test: TestMessagesKeys;
+    errors: Omit<TestMessagesError, "general">;
+    onChange: (change: Partial<TestMessagesKeys>) => void;
+}
+
+export const TestMessages: React.FC<TestEmailsProps> = ({
+    test,
+    onChange,
+    errors,
+}) => {
+
+    const genericChange = <K extends keyof TestMessagesKeys, V extends TestMessagesKeys[K]>(key: K, value: Partial<V>) => {
+        onChange({ [key]: value })
+    }
+
+    return (
+        <Container>
+            <Accordion>
+                <AccordionSection title="After test messages">
+                    <FormField
+                        label="Success message"
+                        type="textarea"
+                        required
+                        value={test.successMessage}
+                        onChange={({target}) => genericChange("successMessage", target.value)}
+                        error={errors.successMessage}
+                    />
+                    <FormField
+                        label="Failure message"
+                        type="textarea"
+                        required
+                        value={test.failureMessage}
+                        onChange={({target}) => genericChange("failureMessage", target.value)}
+                        error={errors.failureMessage}
+                    />
+                </AccordionSection>
+                <AccordionSection title="Success Email">
+                    <EmailForm
+                        email={test.successEmail}
+                        onChange={change => genericChange("successEmail", change)}
+                        errors={errors.successEmail}
+                        />
+                </AccordionSection>
+                <AccordionSection title="Failure Email">
+                    <EmailForm
+                        email={test.failureEmail}
+                        onChange={change => genericChange("failureEmail", change)}
+                        errors={errors.failureEmail}
+                    />
+                </AccordionSection>
+                <AccordionSection title="Email messages wildcards">
+                    <DataTable columns={columns} data={emailTemplates} />
+                </AccordionSection>
+            </Accordion>
+        </Container>
+    );
+};
