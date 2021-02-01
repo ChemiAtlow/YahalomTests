ff059bcf5a8679ac1a66e0c10f9981e0d1fab4bb
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-02-01 20:45:37 +0200

    Generate autocomplete

---
+++ client/src/components/Forms/Autocomplete.tsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from 'react';
+import { FormField } from '.';
+import { useClickOutside } from '../../hooks';
+import './Autocomplete.scoped.scss';
+
+interface AutocompleteProps
+    extends Omit<React.ComponentProps<typeof FormField>, 'search' | 'onChange'> {
+    value: string;
+    options: string[];
+    onChange: (change: string) => void;
+}
+
+const Autocomplete: React.FC<AutocompleteProps> = ({ options, onChange, value, ...rest }) => {
+    const inputRef = useRef<HTMLDivElement>(null);
+    const [showOptions, setShowOptions] = useState(false);
+    const [filteredOptions, setFilteredOptions] = useState<string[]>([]);
+    const [activeOption, setActiveOption] = useState(0);
+    const onValueChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = target;
+        const filteredOptions = options.filter((opt) => new RegExp(value, 'i').test(opt));
+        setFilteredOptions(filteredOptions);
+        setShowOptions(true);
+        onChange(value);
+    };
+    const clickOption = (selected: React.MouseEvent<HTMLLIElement> | string) => {
+        setShowOptions(false);
+        setFilteredOptions([]);
+        setActiveOption(0);
+        onChange(typeof selected === 'string' ? selected : selected.currentTarget.innerText);
+    };
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.code === 'Enter') {
+            clickOption(filteredOptions[activeOption]);
+        } else if (e.code === 'ArrowUp') {
+            setActiveOption(activeOption !== 0 ? activeOption - 1 : filteredOptions.length - 1);
+        } else if (e.code === 'ArrowDown') {
+            setActiveOption(activeOption !== filteredOptions.length - 1 ? activeOption + 1 : 0);
+        }
+    };
+    const optionsRef = useClickOutside<HTMLUListElement>({
+        callback: () => setShowOptions(false),
+        activate: showOptions,
+        ignoredElements: [inputRef],
+    });
+    return (
+        <>
+            <FormField
+                ref={inputRef}
+                {...rest}
+                onKeyDown={onKeyDown}
+                onChange={onValueChange}
+                value={value}
+                search
+            />
+            {showOptions && value && (
+                <ul className="autocomplete-options" ref={optionsRef}>
+                    {filteredOptions.length ? (
+                        filteredOptions.map((optionName, index) => {
+                            return (
+                                <li
+                                    className={`autocomplete-options__item ${
+                                        index === activeOption ? 'active' : ''
+                                    }`}
+                                    key={optionName}
+                                    onClick={clickOption}>
+                                    {optionName}
+                                </li>
+                            );
+                        })
+                    ) : (
+                        <li className="autocomplete-options__item empty">No options!</li>
+                    )}
+                </ul>
+            )}
+        </>
+    );
+};
+
+export default Autocomplete;
