0a40e6e38f787b81be9e3e81293e4cd76f334514
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-02-05 07:58:09 +0200

    add request start log

--- server/src/middleware/reqId.middleware.ts
+++ server/src/middleware/reqId.middleware.ts
@@ -1,7 +1,11 @@
 import { models } from '@yahalom-tests/common';
 import { NextFunction, Request, Response } from 'express';
+import { appLoggerService } from '../services';
 
 export function assignId(req: Request, res: Response, next: NextFunction) {
     req.id = models.classes.Guid.newGuid();
+    if (req.method !== "OPTIONS") {
+        appLoggerService.info(`${req.method} - Req ${req.id} has begun to: ${req.path}.`);
+    }
     next();
 }
