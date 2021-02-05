ae2cac8dd53993a8e8b61cf0de2c419cf7e1c50f
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-02-05 09:07:36 +0200

    fixes

--- server/src/services/appLogger.service.ts
+++ server/src/services/appLogger.service.ts
@@ -1,37 +1,20 @@
 import { join } from 'path';
 import { createLogger, transports, format } from 'winston';
 
-const { combine, timestamp, printf, uncolorize, colorize } = format;
+const { combine, timestamp, printf, json, colorize } = format;
 
 const logDirectory = join(__dirname, '../../logs');
 
 // app loger config
 export const appLogger = createLogger({
     silent: process.env.NODE_ENV === 'test',
-    format: combine(
-        timestamp(),
-        printf((msg) =>
-            colorize({
-                colors: {
-                    error: 'bold redBG white',
-                    warn: 'bold yellow',
-                    info: 'blue',
-                    verbose: 'cyan',
-                    debug: 'gray',
-                },
-            }).colorize(
-                msg.level,
-                `${msg.timestamp} - [${msg.level.toUpperCase()}]: ${msg.message}`
-            )
-        )
-    ),
     transports: [
         new transports.File({
             level: 'verbose',
             filename: 'info.log',
             dirname: logDirectory,
             handleExceptions: true,
-            format: combine(uncolorize()),
+            format: combine(timestamp(), json()),
             maxsize: 5242880,
             maxFiles: 25,
         }),
@@ -42,6 +25,23 @@ if (process.env.NODE_ENV !== "production") {
     appLogger.add( new transports.Console({
         level: 'debug',
         handleExceptions: true,
+        format: combine(
+            timestamp(),
+            printf((msg) =>
+                colorize({
+                    colors: {
+                        error: 'bold redBG white',
+                        warn: 'bold yellow',
+                        info: 'blue',
+                        verbose: 'cyan',
+                        debug: 'gray',
+                    },
+                }).colorize(
+                    msg.level,
+                    `${msg.timestamp} - [${msg.level.toUpperCase()}]: ${msg.message}`
+                )
+            )
+        ),
     }));
 }
 
